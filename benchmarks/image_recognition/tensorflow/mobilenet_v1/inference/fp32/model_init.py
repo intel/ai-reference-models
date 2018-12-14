3355f530ed0476680a341717b4e98351e099a715
Dina Suehiro Jones <dina.s.jones@intel.com>
2018-12-14 14:46:28 -0800

    Add MobileNet V1 FP32 inference (throughput and latency) (#72)
    * Add MobileNet V1 FP32 inference throughput and latency
    
    * Update README to get checkpoints from TensorFlow rather than gcloud
    
    * Updates based on review feedback from Wafaa
    
    * start script update
    
    * Renaming directories from mobilenetv1 to mobilenet_v1

---
+++ benchmarks/image_recognition/tensorflow/mobilenet_v1/inference/fp32/model_init.py
@@ -0,0 +1,71 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+import os
+from common.base_model_init import BaseModelInitializer
+
+os.environ["KMP_BLOCKTIME"] = "1"
+os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+
+
+class ModelInitializer(BaseModelInitializer):
+    """ Model initializer for MobileNet V1 FP32 inference """
+
+    def __init__(self, args, custom_args=[], platform_util=None):
+        self.args = args
+        self.custom_args = custom_args
+
+        # use default batch size if -1
+        if self.args.batch_size == -1:
+            self.args.batch_size = 128
+
+        self.args.num_inter_threads = 2
+        self.args.num_intra_threads = platform_util.num_cores_per_socket()
+
+        if not self.args.single_socket:
+            self.args.num_intra_threads *= platform_util.num_cpu_sockets()
+
+        script_path = os.path.join(args.intelai_models, args.mode,
+                                   args.platform, "eval_image_classifier.py")
+        self.command_prefix = "python {}".format(script_path)
+
+        if self.args.single_socket:
+            self.command_prefix = "numactl --cpunodebind={} -l {}".format(
+                str(self.args.socket_id), self.command_prefix)
+
+        os.environ["OMP_NUM_THREADS"] = str(self.args.num_intra_threads)
+
+        self.command_prefix = ("{prefix} "
+                               "--dataset_name imagenet "
+                               "--checkpoint_path {checkpoint} "
+                               "--dataset_dir {dataset} "
+                               "--dataset_split_name=validation "
+                               "--clone_on_cpu=True "
+                               "--model_name {model} "
+                               "--inter_op_parallelism_threads {inter} "
+                               "--intra_op_parallelism_threads {intra} "
+                               "--batch_size {bz}").format(
+            prefix=self.command_prefix, checkpoint=self.args.checkpoint,
+            dataset=self.args.data_location, model=self.args.model_name,
+            inter=self.args.num_inter_threads,
+            intra=self.args.num_intra_threads, bz=self.args.batch_size)
+
+    def run(self):
+        self.run_command(self.command_prefix)
