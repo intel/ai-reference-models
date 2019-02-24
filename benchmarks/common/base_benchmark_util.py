3049df05ba45f67458cc001e1144cb9c4398170b
Karthik Vadla <karthik.vadla@intel.com>
2019-02-24 13:05:23 -0800

    Refactor (#194)

--- benchmarks/common/base_benchmark_util.py
+++ benchmarks/common/base_benchmark_util.py
@@ -174,6 +174,28 @@ class BaseBenchmarkUtil(object):
                               format(model_source_dir))
             self.check_for_link("model source directory", model_source_dir)
 
+        # check checkpoint location
+        checkpoint_dir = args.checkpoint
+        if checkpoint_dir is not None:
+            if not os.path.exists(checkpoint_dir):
+                raise IOError("The checkpoint location {} does not exist.".
+                              format(checkpoint_dir))
+            elif not os.path.isdir(checkpoint_dir):
+                raise IOError("The checkpoint location {} is not a directory.".
+                              format(checkpoint_dir))
+            self.check_for_link("checkpoint directory", checkpoint_dir)
+
+        # check if input graph file exists
+        input_graph = args.input_graph
+        if input_graph is not None:
+            if not os.path.exists(input_graph):
+                raise IOError("The input graph {} does not exist.".
+                              format(input_graph))
+            if not os.path.isfile(input_graph):
+                raise IOError("The input graph {} must be a file.".
+                              format(input_graph))
+            self.check_for_link("input graph", input_graph)
+
         # check model_name exists
         if not args.model_name:
             raise ValueError("The model name is not valid")
--- benchmarks/launch_benchmark.py
+++ benchmarks/launch_benchmark.py
@@ -78,28 +78,6 @@ class LaunchBenchmark(base_benchmark_util.BaseBenchmarkUtil):
             raise ValueError("The specified framework is not supported: {}".
                              format(args.framework))
 
-        # check checkpoint location
-        checkpoint_dir = args.checkpoint
-        if checkpoint_dir is not None:
-            if not os.path.exists(checkpoint_dir):
-                raise IOError("The checkpoint location {} does not exist.".
-                              format(checkpoint_dir))
-            elif not os.path.isdir(checkpoint_dir):
-                raise IOError("The checkpoint location {} is not a directory.".
-                              format(checkpoint_dir))
-            self.check_for_link("checkpoint directory", checkpoint_dir)
-
-        # check if input graph file exists
-        input_graph = args.input_graph
-        if input_graph is not None:
-            if not os.path.exists(input_graph):
-                raise IOError("The input graph {} does not exist.".
-                              format(input_graph))
-            if not os.path.isfile(input_graph):
-                raise IOError("The input graph {} must be a file.".
-                              format(input_graph))
-            self.check_for_link("input graph", input_graph)
-
         # if neither benchmark_only or accuracy_only are specified, then enable
         # benchmark_only as the default
         if not args.benchmark_only and not args.accuracy_only:
