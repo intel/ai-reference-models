d6e141984bdbfe2aac7f1d50eb0bb5b4a3916b62
Melanie Buehler <mhbuehler@gmail.com>
2019-03-22 15:29:16 -0700

    Adds validation for directory input (#245)

--- docs/object_detection/tensorflow_serving/rfcn-benchmark.py
+++ docs/object_detection/tensorflow_serving/rfcn-benchmark.py
@@ -28,6 +28,26 @@ from PIL import Image
 import tensorflow as tf
 from object_detection.utils.visualization_utils import visualize_boxes_and_labels_on_image_array
 
+
+def check_for_link(value):
+    """
+    Throws an error if the specified path is a link. os.islink returns
+    True for sym links.  For files, we also look at the number of links in
+    os.stat() to determine if it's a hard link.
+    """
+    if os.path.islink(value) or \
+            (os.path.isfile(value) and os.stat(value).st_nlink > 1):
+        raise argparse.ArgumentTypeError("{} cannot be a link.".format(value))
+
+def check_valid_folder(value):
+    """verifies filename exists and isn't a link"""
+    if value is not None:
+        if not os.path.isdir(value):
+            raise argparse.ArgumentTypeError("{} does not exist or is not a directory.".
+                                    format(value))
+        check_for_link(value)
+    return value
+
 def get_random_image(image_dir):
     image_path = os.path.join(image_dir, random.choice(os.listdir(image_dir)))
     image = Image.open(image_path)
@@ -59,11 +79,12 @@ def benchmark(batch_size=1, num_iteration=20, warm_up_iteration=10):
     
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--images_path", required=True, help="Path to COCO validation directory")
+    ap.add_argument("-i", "--images_path", type=check_valid_folder, required=True, 
+                    help="Path to COCO validation directory")
     args = vars(ap.parse_args())
     
     SERVER_URL = 'http://localhost:8501/v1/models/rfcn:predict'
-    IMAGES_PATH = args['images_path'] # Edit this to your COCO validation directory
+    IMAGES_PATH = args['images_path']
 
     print('\n SERVER_URL: {} \n IMAGES_PATH: {}'.format(SERVER_URL, IMAGES_PATH))
     
