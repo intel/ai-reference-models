eaf3be8e35ecfa741c69a42bb2ed5796bc5ef03e
Dina Suehiro Jones <dina.s.jones@intel.com>
2019-03-29 18:47:34 -0700

    Add Inception ResNet v2 Int8 inference benchmarking scripts (#160)
    * Add Inception ResNet v2 Int8 inference benchmarking scripts
    
    * Update model_init due to base class changes
    
    * Update to use python_exe in the model_init
    
    * Update README with the new docker image

---
+++ benchmarks/image_recognition/tensorflow/inception_resnet_v2/inference/int8/model_init.py
@@ -0,0 +1,73 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from common.base_model_init import BaseModelInitializer, set_env_var
+
+import os
+
+
+class ModelInitializer(BaseModelInitializer):
+    """Model initializer for Inception ResNet V2 int8 inference"""
+
+    def __init__(self, args, custom_args=[], platform_util=None):
+        super(ModelInitializer, self).__init__(args, custom_args, platform_util)
+        self.set_kmp_vars()
+        self.cmd = self.get_numactl_command(self.args.socket_id) + "{} ".format(self.python_exe)
+
+        # use default batch size if -1
+        if self.args.batch_size == -1:
+            self.args.batch_size = 128
+
+        # set num_inter_threads and num_intra_threads
+        self.set_num_inter_intra_threads()
+
+        if self.args.benchmark_only:
+            run_script = os.path.join(self.args.intelai_models,
+                                      "eval_image_classifier_benchmark.py")
+
+            cmd_args = " --input-graph=" + self.args.input_graph + \
+                " --inter-op-parallelism-threads=" + \
+                str(self.args.num_inter_threads) + \
+                " --intra-op-parallelism-threads=" + \
+                str(self.args.num_intra_threads) + \
+                " --batch-size=" + str(self.args.batch_size)
+        elif self.args.accuracy_only:
+            run_script = os.path.join(self.args.intelai_models,
+                                      "eval_image_classifier_accuracy.py")
+
+            cmd_args = " --input_graph=" + self.args.input_graph + \
+                " --data_location=" + self.args.data_location + \
+                " --input_height=299" + " --input_width=299" + \
+                " --num_inter_threads=" + str(self.args.num_inter_threads) + \
+                " --num_intra_threads=" + str(self.args.num_intra_threads) + \
+                " --output_layer=InceptionResnetV2/Logits/Predictions" + \
+                " --batch_size=" + str(self.args.batch_size)
+
+        set_env_var("OMP_NUM_THREADS", self.args.num_intra_threads)
+        self.cmd = self.cmd + run_script + cmd_args
+
+    def run(self):
+        """run command to enable model benchmark or accuracy measurement"""
+
+        if self.cmd:
+            self.run_command(self.cmd)
