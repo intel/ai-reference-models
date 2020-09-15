0ccc9095f749d9e8442b3f609bf25c28f0facdb2
Jones, Dina S <dina.s.jones@intel.com>
2020-09-15 16:45:49 -0700

    Add back 3D U-Net FP32 inference

---
+++ benchmarks/image_segmentation/tensorflow/3d_unet/inference/fp32/model_init.py
@@ -0,0 +1,57 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+
+import os
+
+from common.base_model_init import BaseModelInitializer
+from common.base_model_init import set_env_var
+
+
+class ModelInitializer(BaseModelInitializer):
+    """ Model initializer for 3D UNet"""
+
+    def __init__(self, args, custom_args=[], platform_util=None):
+        super(ModelInitializer, self).__init__(args, custom_args, platform_util)
+
+        self.set_num_inter_intra_threads()
+        set_env_var("OMP_NUM_THREADS", self.args.num_intra_threads)
+
+        # Set KMP env vars, if they haven't already been set
+        config_file_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "config.json")
+        self.set_kmp_vars(config_file_path)
+        set_env_var("KMP_HW_SUBSET",
+                    "{}c,1T".format(self.args.num_intra_threads))
+        script_path = os.path.join(self.args.intelai_models, self.args.mode,
+                                   self.args.precision, "brats", "predict.py")
+
+        # add numactl prefix to the command
+        self.command_prefix = self.get_command_prefix(self.args.socket_id) + \
+            "python " + script_path
+
+        # add additional args to the command
+        self.command_prefix += \
+            " --inter {} --intra {} --nw 1 --nb 5 --bs {}".\
+            format(self.args.num_inter_threads, self.args.num_intra_threads,
+                   self.args.batch_size)
+
+    def run(self):
+        self.run_command(self.command_prefix)
---
+++ models/image_segmentation/tensorflow/3d_unet/inference/fp32/unet3d/normalize.py
@@ -0,0 +1,88 @@
+import os
+import collections
+
+import numpy as np
+from nilearn.image import new_img_like
+
+from unet3d.utils.utils import resize
+from .utils import crop_img, crop_img_to, read_image
+
+
+def find_downsized_info(training_data_files, input_shape):
+    foreground = get_complete_foreground(training_data_files)
+    crop_slices = crop_img(foreground, return_slices=True, copy=True)
+    cropped = crop_img_to(foreground, crop_slices, copy=True)
+    final_image = resize(cropped, new_shape=input_shape, interpolation="nearest")
+    return crop_slices, final_image.affine, final_image.header
+
+
+def get_cropping_parameters(in_files):
+    foreground = get_complete_foreground(in_files)
+    return crop_img(foreground, return_slices=True, copy=True)
+
+
+def reslice_image_set(in_files, image_shape, out_files=None, label_indices=None):
+    if label_indices is None:
+        label_indices = []
+    elif not isinstance(label_indices, collections.Iterable) or isinstance(label_indices, str):
+        label_indices = [label_indices]
+
+    crop_slices = get_cropping_parameters([in_files])
+    images = list()
+    for index, in_file in enumerate(in_files):
+        interpolation = "continuous"
+        if index in label_indices:
+            interpolation = "nearest"
+        images.append(read_image(in_file, image_shape=image_shape, crop=crop_slices, interpolation=interpolation))
+    if out_files:
+        for image, out_file in zip(images, out_files):
+            image.to_filename(out_file)
+        return [os.path.abspath(out_file) for out_file in out_files]
+    else:
+        return images
+
+
+def get_complete_foreground(training_data_files):
+    for i, set_of_files in enumerate(training_data_files):
+        subject_foreground = get_foreground_from_set_of_files(set_of_files)
+        if i == 0:
+            foreground = subject_foreground
+        else:
+            foreground[subject_foreground > 0] = 1
+
+    return new_img_like(read_image(training_data_files[0][-1]), foreground)
+
+
+def get_foreground_from_set_of_files(set_of_files, background_value=0, tolerance=0.00001):
+    for i, image_file in enumerate(set_of_files):
+        image = read_image(image_file)
+        is_foreground = np.logical_or(image.get_data() < (background_value - tolerance),
+                                      image.get_data() > (background_value + tolerance))
+        if i == 0:
+            foreground = np.zeros(is_foreground.shape, dtype=np.uint8)
+
+        foreground[is_foreground] = 1
+
+    return foreground
+
+
+def normalize_data(data, mean, std):
+    data -= mean[:, np.newaxis, np.newaxis, np.newaxis]
+    data /= std[:, np.newaxis, np.newaxis, np.newaxis]
+    return data
+
+
+def normalize_data_storage(data_storage):
+    means = list()
+    stds = list()
+    for index in range(data_storage.shape[0]):
+        data = data_storage[index]
+        means.append(data.mean(axis=(1, 2, 3)))
+        stds.append(data.std(axis=(1, 2, 3)))
+    mean = np.asarray(means).mean(axis=0)
+    std = np.asarray(stds).mean(axis=0)
+    for index in range(data_storage.shape[0]):
+        data_storage[index] = normalize_data(data_storage[index], mean, std)
+    return data_storage
+
+
