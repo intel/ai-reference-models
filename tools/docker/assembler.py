b635105f6793cf8af5f51de2324aa52184ad72a3
wafaa.s.taie <wstaie@mlp-ra-skx-17.ra.intel.com>
2020-10-29 15:48:48 -0700

    model-builder generate-dockerfile all not to delete files in dockerfiles/ml

--- tools/docker/assembler.py
+++ tools/docker/assembler.py
@@ -1121,7 +1121,7 @@ def main(argv):
       try:
         if os.path.isfile(file_path):
           os.unlink(file_path)
-        elif os.path.isdir(file_path):
+        elif os.path.isdir(file_path) and file_path != 'models/dockerfiles/ml':
           shutil.rmtree(file_path)
       except Exception as e:
         print(e)
