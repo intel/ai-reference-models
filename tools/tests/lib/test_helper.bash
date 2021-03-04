2263ddde93c7f60f41b2e3e8a75ff3e20a66de1d
Taie, Wafaa S <wafaa.s.taie@intel.com>
2021-03-04 12:57:29 -0800

    K8s packages tests: Checks if username has underscore before creating a namespace

--- tools/tests/lib/test_helper.bash
+++ tools/tests/lib/test_helper.bash
@@ -6,8 +6,17 @@ load "lib/detik"
 DETIK_CLIENT_NAME="kubectl"
 
 # create namespace $USER if not exist
-kubectl get ns -oname | grep $USER || kubectl create ns $USER
-$DETIK_CLIENT_NAME config set-context $($DETIK_CLIENT_NAME config current-context) --namespace=$USER
+user_name="$USER"
+echo "-------------------------------------------"
+if [[ $user_name =~ _ ]]; then
+  user_name="${user_name//_/-}"
+  echo " "
+  echo "*** Cannot create namespace for USERNAME $USER contains '_'"
+  echo "*** Tests will use USERNAME $user_name instead ..."
+fi
+
+(kubectl get ns -oname | grep $user_name || kubectl create ns $user_name) && $DETIK_CLIENT_NAME config set-context $($DETIK_CLIENT_NAME config current-context) --namespace=$user_name
+echo " "
 
 last_modified ()
 {
@@ -20,5 +29,13 @@ last_modified ()
             expr $(date +%s) - $(stat -c %Y $1)
         ;;
     esac
+}
+
+launcher_logs()
+{
+  local _launcher_pod=$($DETIK_CLIENT_NAME get pods --no-headers -oname | grep launcher | sed 's^pod/^^')
+  if [[ -n $_launcher_pod ]]; then
+    $DETIK_CLIENT_NAME logs $_launcher_pod | tail -5
+  fi
 }
 # *** tests for generate-deployment ***
