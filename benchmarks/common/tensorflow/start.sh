83d680448fc50ed08ff2e074e58fe95d03e5c952
Varghese, Jojimon <jojimon.varghese@intel.com>
2021-05-05 09:45:56 -0700

    Update README file and fix few errors.

--- benchmarks/common/tensorflow/start.sh
+++ benchmarks/common/tensorflow/start.sh
@@ -555,7 +555,7 @@ function dien() {
   if [ ${MODE} == "inference" ]; then
     if [ ${PRECISION} == "fp32" ] || [ ${PRECISION} == "bfloat16" ]; then
       if [ ${NOINSTALL} != "True" ]; then
-        python3 -m pip install -r ${MOUNT_BENCHMARK}/recommendation/tensorflow/DIEN/requirements.txt
+        python3 -m pip install -r ${MOUNT_BENCHMARK}/recommendation/tensorflow/dien/requirements.txt
       fi
       dien_options
       CMD=${CMD} run_model
@@ -567,7 +567,7 @@ function dien() {
   elif [ ${MODE} == "training" ]; then
     if [ ${PRECISION} == "fp32" ]; then
       if [ ${NOINSTALL} != "True" ]; then
-        python3 -m pip install -r ${MOUNT_BENCHMARK}/recommendation/tensorflow/DIEN/requirements.txt
+        python3 -m pip install -r ${MOUNT_BENCHMARK}/recommendation/tensorflow/dien/requirements.txt
       fi
       dien_options
       CMD=${CMD} run_model
