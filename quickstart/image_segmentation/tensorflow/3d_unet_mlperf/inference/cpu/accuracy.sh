35cdedd3953c3b7086d606966fe823704c64c07b
Wafaa Taie <wafaa.s.taie@intel.com>
2021-09-02 12:29:32 -0700

    Add 3D U-Net Inference container package for TF SPR (#110)
    * add 3d_unet_mlperf container package scripts

---
+++ quickstart/image_segmentation/tensorflow/3d_unet_mlperf/inference/cpu/accuracy.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+MODEL_DIR=${MODEL_DIR-$PWD}
+
+if [ -z "${OUTPUT_DIR}" ]; then
+  echo "The required environment variable OUTPUT_DIR has not been set"
+  exit 1
+fi
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+if [ -z "${PRECISION}" ]; then
+  echo "The required environment variable PRECISION has not been set"
+  echo "Please set PRECISION to fp32, int8, or bfloat16."
+  exit 1
+fi
+
+if [ -z "${DATASET_DIR}" ]; then
+  echo "The required environment variable DATASET_DIR has not been set"
+  exit 1
+fi
+
+if [ ! -d "${DATASET_DIR}" ]; then
+  echo "The DATASET_DIR '${DATASET_DIR}' does not exist"
+  exit 1
+fi
+
+if [ -z "${PRETRAINED_MODEL}" ]; then
+    if [[ $PRECISION == "int8" ]]; then
+        PRETRAINED_MODEL="${MODEL_DIR}/pretrained_model/3dunet_int8_fully_quantized_perchannel.pb"
+    elif [[ $PRECISION == "bfloat16" || $PRECISION == "fp32" ]]; then
+        PRETRAINED_MODEL="${MODEL_DIR}/pretrained_model/3dunet_dynamic_ndhwc.pb"
+    else
+        echo "The specified precision '${PRECISION}' is unsupported."
+        echo "Supported precisions are: fp32, bfloat16, and int8"
+        exit 1
+    fi
+    if [[ ! -f "${PRETRAINED_MODEL}" ]]; then
+    echo "The pretrained model could not be found. Please set the PRETRAINED_MODEL env var to point to the frozen graph file."
+    exit 1
+    fi
+elif [[ ! -f "${PRETRAINED_MODEL}" ]]; then
+  echo "The file specified by the PRETRAINED_MODEL environment variable (${PRETRAINED_MODEL}) does not exist."
+  exit 1
+fi
+
+MODE="inference"
+BATCH_SIZE="1"
+source "${MODEL_DIR}/quickstart/common/utils.sh"
+_command python ${MODEL_DIR}/benchmarks/launch_benchmark.py \
+  --model-name=3d_unet_mlperf \
+  --precision ${PRECISION} \
+  --mode=${MODE} \
+  --framework tensorflow \
+  --in-graph ${PRETRAINED_MODEL} \
+  --data-location=${DATASET_DIR} \
+  --output-dir ${OUTPUT_DIR} \
+  --batch-size ${BATCH_SIZE} \
+  --accuracy-only \
+  $@ \
+  -- DEBIAN_FRONTEND=noninteractive
