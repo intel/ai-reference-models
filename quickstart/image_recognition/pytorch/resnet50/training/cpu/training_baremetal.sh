7c873fbc90ca773ce52a261a8b74a9d39a278a34
XiaobingZhang <xiaobing.zhang@intel.com>
2021-11-03 09:02:54 +0800

    update pytorch resnet model code for ipex api change (#187)

--- quickstart/image_recognition/pytorch/resnet50/training/cpu/training_baremetal.sh
+++ quickstart/image_recognition/pytorch/resnet50/training/cpu/training_baremetal.sh
@@ -85,7 +85,7 @@ BATCH_SIZE=128
 
 rm -rf ./resnet50_training_log_*
 
-python -m intel_extension_for_pytorch.launch \
+python -m intel_extension_for_pytorch.cpu.launch \
     --use_default_allocator \
     --ninstances 1 \
     --ncore_per_instance ${CORES_PER_INSTANCE} \
