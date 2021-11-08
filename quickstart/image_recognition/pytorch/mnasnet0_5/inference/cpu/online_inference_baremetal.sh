6384f6611707ed5e0d9a3c14a156955b1531bab7
blzheng <beilei.zheng@intel.com>
2021-11-08 09:06:37 +0800

    Enable PyTorch image classification models inference (#178)
    * Enable inference for PyTorch vgg11
    
    * Enable inference for PyTorch vgg11_bn
    
    * Enable inference for PyTorch googlenet
    
    * Enable inference for PyTorch resnet101
    
    * Enable inference for PyTorch resnet152
    
    * Enable inference for PyTorch resnext50_32x4d
    
    * Enable inference for PyTorch wide_resnet50_2
    
    * Enable inference for PyTorch wide_resnet101_2
    
    * Enable inference for PyTorch mnasnet0_5
    
    * Enable inference for PyTorch mnasnet1_0
    
    * Enable inference for PyTorch inception_v3
    
    * update readme
    
    * update code according to IPEX new API
    
    * move the warm up iterations out of inference
    
    * add a new line at the end of files
    
    * add error handling for unsupported precision

---
+++ quickstart/image_recognition/pytorch/mnasnet0_5/inference/cpu/online_inference_baremetal.sh
@@ -0,0 +1,99 @@
+#
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+MODEL_DIR=${MODEL_DIR-$PWD}
+
+if [ ! -e "${MODEL_DIR}/models/image_recognition/pytorch/common/inference.py" ]; then
+  echo "Could not find the script of inference.py. Please set environment variable '\${MODEL_DIR}'."
+  echo "From which the inference.py exist at the: \${MODEL_DIR}/models/image_recognition/pytorch/common/inference.py"
+  exit 1
+fi
+
+if [ -z "${DATASET_DIR}" ]; then
+  echo "The required environment variable DATASET_DIR has not been set"
+  exit 1
+fi
+
+if [ ! -d "${DATASET_DIR}" ]; then
+  echo "The DATASET_DIR '${DATASET_DIR}' does not exist"
+  exit 1
+fi
+
+if [ -z "${OUTPUT_DIR}" ]; then
+  echo "The required environment variable OUTPUT_DIR has not been set"
+  exit 1
+fi
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+ARGS=""
+PRECISION="fp32"
+if [ "$1" == "bf16" ]; then
+  ARGS="$ARGS --precision bf16"
+  PRECISION="bf16"
+  echo "### running bf16 datatype"
+elif [ "$1" == "fp32" ]; then
+  ARGS="$ARGS --precision fp32"
+  echo "### running fp32 datatype"
+else
+  echo "The specified precision '$1' is unsupported."
+  echo "Supported precisions are: fp32 and bf16"
+  exit 1
+fi
+
+export DNNL_PRIMITIVE_CACHE_CAPACITY=1024
+export KMP_BLOCKTIME=1
+export KMP_AFFINITY=granularity=fine,compact,1,0
+
+BATCH_SIZE=1
+
+python -m intel_extension_for_pytorch.cpu.launch \
+  --use_default_allocator \
+  --latency_mode \
+  --log_path=${OUTPUT_DIR} \
+  --log_file_prefix="mnasnet0_5_latency_log_${PRECISION}" \
+  ${MODEL_DIR}/models/image_recognition/pytorch/common/inference.py \
+  --data_path ${DATASET_DIR} \
+  --arch mnasnet0_5 \
+  --batch_size $BATCH_SIZE \
+  --ipex \
+  --jit \
+  -j 0 \
+  $ARGS 
+
+wait
+
+CORES=`lscpu | grep Core | awk '{print $4}'`
+CORES_PER_INSTANCE=4
+
+INSTANCES_THROUGHPUT_BENCHMARK_PER_SOCKET=`expr $CORES / $CORES_PER_INSTANCE`
+
+throughput=$(grep 'Throughput:' ${OUTPUT_DIR}/mnasnet0_5_latency_log_${PRECISION}_* |sed -e 's/.*Throughput//;s/[^0-9.]//g' |awk -v INSTANCES_PER_SOCKET=$INSTANCES_THROUGHPUT_BENCHMARK_PER_SOCKET '
+BEGIN {
+        sum = 0;
+        i = 0;
+      }
+      {
+        sum = sum + $1;
+        i++;
+      }
+END   {
+        sum = sum / i * INSTANCES_PER_SOCKET;
+        printf("%.2f", sum);
+}')
+echo "mnasnet0_5;"latency";${PRECISION};${BATCH_SIZE};${throughput}" | tee -a ${OUTPUT_DIR}/summary.log
