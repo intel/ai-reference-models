ebef371f560fb4074c221da37cdaf8d02c1b8795
jiayisunx <jiayi.sun@intel.com>
2021-11-24 16:16:53 +0800

    enable jemalloc for pytorch maskrcnn model (#244)

--- quickstart/object_detection/pytorch/maskrcnn/inference/cpu/inference_realtime_baremetal.sh
+++ quickstart/object_detection/pytorch/maskrcnn/inference/cpu/inference_realtime_baremetal.sh
@@ -73,7 +73,7 @@ BATCH_SIZE=1
 rm -rf ${OUTPUT_DIR}/latency_log*
 
 python -m intel_extension_for_pytorch.cpu.launch \
-    --use_default_allocator \
+    --enable_jemalloc \
     --latency_mode \
     ${MODEL_DIR}/models/object_detection/pytorch/maskrcnn/maskrcnn-benchmark/tools/test_net.py \
     $ARGS \
