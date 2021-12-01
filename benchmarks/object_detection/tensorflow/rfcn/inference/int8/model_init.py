d0adaf551b854f139c3cb8c55d9e8f7ece562566
Wafaa Taie <wafaa.s.taie@intel.com>
2021-12-01 03:52:01 +0200

    Fix RFCN on WSL (when numactl not found on the system) (#247)
    * remove numa prefix if numa not found on the system

--- benchmarks/object_detection/tensorflow/rfcn/inference/int8/model_init.py
+++ benchmarks/object_detection/tensorflow/rfcn/inference/int8/model_init.py
@@ -115,7 +115,8 @@ class ModelInitializer(BaseModelInitializer):
 
         set_env_var("OMP_NUM_THREADS", self.args.num_intra_threads)
 
-        if self.args.socket_id != -1:
+        num_numas = self.platform_util.num_numa_nodes
+        if self.args.socket_id != -1 and num_numas > 0:
             self.command.append("numactl")
             if self.args.socket_id:
                 socket_id = self.args.socket_id
