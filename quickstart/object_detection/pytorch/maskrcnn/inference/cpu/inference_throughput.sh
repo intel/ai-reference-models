968ac876dfae9598fedaf55330440fd739c6b3e5
jiayisunx <jiayi.sun@intel.com>
2022-01-17 13:00:02 +0800

    modify the BS of maskrcnn throughput (#356)

--- quickstart/object_detection/pytorch/maskrcnn/inference/cpu/inference_throughput.sh
+++ quickstart/object_detection/pytorch/maskrcnn/inference/cpu/inference_throughput.sh
@@ -73,7 +73,7 @@ export KMP_AFFINITY=granularity=fine,compact,1,0
 export TRAIN=0
 
 PRECISION=$1
-BATCH_SIZE=112
+BATCH_SIZE=168
 
 rm -rf ${OUTPUT_DIR}/maskrcnn_${PRECISION}_inference_throughput*
 
