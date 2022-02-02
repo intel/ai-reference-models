b07b93c2218b72fe1a964d0c43e27ee9c4e92f04
Wafaa Taie <wafaa.s.taie@intel.com>
2022-02-02 13:37:22 -0800

    Update COCO validation dataset instructions for bare metal and docker (#390)
    * update coco dataset instructions for baremetal and docker
    * update coco script and instructions to remove output dir env var

--- datasets/coco/preprocess_coco_val.sh
+++ datasets/coco/preprocess_coco_val.sh
@@ -22,10 +22,9 @@
 # The following vars need to be set:
 #   VAL_IMAGE_DIR: Points to the raw validation images (extracted from val2017.zip)
 #   ANNOTATIONS_DIR: Points to the annotations (extracted from annotations_trainval2017.zip)
-#   OUTPUT_DIR: Path where the TF records file will be written
 #
 # This is intended to be used with the create_coco_tf_record.py script from the
-# TensorFlow Model Garden, commit 1efe98bb8e8d98bbffc703a90d88df15fc2ce906.
+# TensorFlow Model Garden.
 #
 # NOTE: This pre-processes the validation images only
 
@@ -58,9 +57,6 @@ if [[ ! -d "${TF_MODELS_DIR}" ]]; then
   exit 1
 fi
 
-# Create the output directory in case it doesn't already exist
-mkdir -p ${OUTPUT_DIR}
-
 # Checkout the specified branch for the tensorflow/models code
 if [[ ! -z "${TF_MODELS_BRANCH}" ]]; then
   cd ${TF_MODELS_DIR}
@@ -73,8 +69,8 @@ export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 
 # Create empty dir and json for train/test image preprocessing, so that we don't require
 # the user to also download train/test images when all that's needed is validation images.
-EMPTY_DIR=${OUTPUT_DIR}/empty_dir
-EMPTY_ANNOTATIONS=${OUTPUT_DIR}/empty.json
+EMPTY_DIR=${DATASET_DIR}/empty_dir
+EMPTY_ANNOTATIONS=${DATASET_DIR}/empty.json
 mkdir -p ${EMPTY_DIR}
 echo "{ \"images\": {}, \"categories\": {}}" > ${EMPTY_ANNOTATIONS}
 
@@ -86,7 +82,7 @@ python create_coco_tf_record.py --logtostderr \
       --train_annotations_file="${EMPTY_ANNOTATIONS}" \
       --val_annotations_file="${ANNOTATIONS_DIR}/instances_val2017.json" \
       --testdev_annotations_file="${EMPTY_ANNOTATIONS}" \
-      --output_dir="${OUTPUT_DIR}"
+      --output_dir="${DATASET_DIR}"
 
 # remove dummy directory and annotations file
 rm -rf ${EMPTY_DIR}
@@ -94,8 +90,11 @@ rm -rf ${EMPTY_ANNOTATIONS}
 
 # since we only grab the validation dataset, the TF records files for train
 # and test images are size 0. Delete those to prevent confusion.
-rm -f ${OUTPUT_DIR}/coco_testdev.record
-rm -f ${OUTPUT_DIR}/coco_train.record
+rm -f ${DATASET_DIR}/coco_testdev.record
+rm -f ${DATASET_DIR}/coco_train.record
 
-echo "TF records in the output directory:"
-ls -l ${OUTPUT_DIR}
+# create a copy of the TF record file and rename it to be used for the SSD-ResNet34 model
+cp ${DATASET_DIR}/coco_val.record ${DATASET_DIR}/validation-00000-of-00001
+
+echo "TF records is listed in the dataset directory:"
+ls -l ${DATASET_DIR}
