61264f8364cc9289d3c3f020aeac0ffb20c8fa00
Wafaa Taie <wafaa.s.taie@intel.com>
2022-03-15 11:43:57 -0700

    Fix Transformer Language mlperf to add arg --kmp-blocktime (#469)
    * fix transformer mlperf to parse --kmp-blocktime, in case set on the system

--- benchmarks/language_translation/tensorflow/transformer_mlperf/inference/fp32/model_init.py
+++ benchmarks/language_translation/tensorflow/transformer_mlperf/inference/fp32/model_init.py
@@ -32,10 +32,6 @@ class ModelInitializer(BaseModelInitializer):
         self.cmd = self.get_command_prefix(self.args.socket_id)
         self.bleu_params = ""
 
-        # Set KMP env vars, if they haven't already been set
-        config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
-        self.set_kmp_vars(config_file_path)
-
         MODEL_EXEC_DIR = os.path.join(self.args.intelai_models, self.args.mode, self.args.precision)
 
         self.cmd += self.python_exe
@@ -70,9 +66,16 @@ class ModelInitializer(BaseModelInitializer):
         arg_parser.add_argument("--steps", dest='steps',
                                 type=int, default=100,
                                 help="number of steps")
+        arg_parser.add_argument('--kmp-blocktime', dest='kmp_blocktime',
+                            help='number of kmp block time',
+                            type=int, default=1)
 
         self.args = arg_parser.parse_args(self.custom_args,
                                           namespace=self.args)
+
+        # Set KMP env vars, if they haven't already been set
+        config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+        self.set_kmp_vars(config_file_path, kmp_blocktime=str(self.args.kmp_blocktime))
 
         # Model parameter control
         translate_file = os.path.join(self.args.output_dir,
