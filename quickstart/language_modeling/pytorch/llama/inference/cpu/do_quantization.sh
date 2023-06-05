738aa87b8839e1c604a2af66e2ff517406dd8fa4
jianan-gu <jianan.gu@intel.com>
2023-06-05 13:07:37 +0800

    Adding llm models inference generation for llama/gptj/bloom and lora training for llama (#1259)
    * draft adding llama training and inference
    
    * llm model common enabling
    
    * Update README.md
    
    * model refine
    
    * Update finetune.py
    
    * Update prompter.py
    
    ---------
    
    Co-authored-by: liangan1 <liangang.zhang@intel.com>
    Co-authored-by: leslie-fang-intel <leslie.fang@intel.com>

---
+++ quickstart/language_modeling/pytorch/llama/inference/cpu/do_quantization.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+ARGS=""
+
+export DNNL_PRIMITIVE_CACHE_CAPACITY=1024
+#export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+if [ -z "${OUTPUT_DIR}" ]; then
+  echo "The required environment variable OUTPUT_DIR has not been set, please create the output path and set it to OUTPUT_DIR"
+  exit 1
+fi
+path="ipex"
+ARGS="$ARGS --output_dir ${OUTPUT_DIR}  --lambada --jit"
+echo "### running with intel extension for pytorch"
+
+if [[ "$1" == "int8-fp32" ]]
+then
+    precision="int8-fp32"
+    ARGS="$ARGS --dtype 'int8' "
+    echo "### running int8-fp32 mode"
+elif [[ "$1" == "int8-bf16" ]]
+then
+    precision="int8-bf16"
+    ARGS="$ARGS --dtype 'int8' --int8_bf16_mixed "
+    echo "### running int8-bf16 mode"
+else
+    echo "The specified precision '$1' is unsupported."
+    echo "Supported precisions are: int8-fp32, int8-bf16"
+    exit 1
+fi
+
+if [[ "$2" == "default" ]]
+then
+    ARGS="$ARGS --ipex_static_quantize "
+    echo "### ipex_static_quantize"
+elif [[ "$2" == "sq" ]]
+then
+    ARGS="$ARGS --ipex_smooth_quant "
+    echo "###  ipex_smooth_quant"
+else
+    echo "The specified precision '$2' is unsupported."
+    echo "Supported precisions are: default, sq"
+    exit 1
+fi
+
+
+
+mode="jit"
+ARGS="$ARGS --jit"
+echo "### running with jit mode"
+
+
+FINETUNED_MODEL=${FINETUNED_MODEL:-"'decapoda-research/llama-7b-hf'"}
+
+EVAL_SCRIPT=${EVAL_SCRIPT:-"../../../../../../models/language_modeling/pytorch/llama/inference/cpu/run_llm.py"}
+WORK_SPACE=${WORK_SPACE:-${OUTPUT_DIR}}
+rm -rf ${OUTPUT_DIR}/latency_log*
+python -m intel_extension_for_pytorch.cpu.launch --node_id 0 --enable_tcmalloc --log_path=${OUTPUT_DIR} --log_file_prefix="./latency_log_${precision}_${mode}" \
+  ${EVAL_SCRIPT} $ARGS \
+  --model-name-or-path   ${FINETUNED_MODEL} \
+
