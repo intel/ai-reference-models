7fd01a7d5bb39ccad1cae7b2e352cd0a6f02af13
Srikanth Ramakrishna <srikanth.ramakrishna@intel.com>
2023-08-16 16:16:33 -0700

    p0 ipex stable diffusion (#1424)
    * add ipex stable diffusion
    
    * add tests and dependencies
    
    * add save image path
    
    * separate commits
    
    * add devcatalog and remove export vars
    
    * rename diffusion to generative-ai
    
    * modify names of folder
    
    * add license header
    
    * indent the yaml file

---
+++ quickstart/generative-ai/pytorch/stable_diffusion/inference/gpu/online_inference.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+MODEL_DIR=${MODEL_DIR-$PWD}
+BATCH_SIZE=${BATCH_SIZE-1}
+PRECISION=${PRECISION-fp32}
+
+if [[ -z $OUTPUT_DIR ]]; then
+  echo "The required environment variable OUTPUT_DIR has not been set"
+  exit 1
+fi
+
+echo "Stable Diffusion Inference Inference"
+if [[ ${PRECISION} == "fp32" ]]; then
+
+python -u ${MODEL_DIR}/models/generative-ai/pytorch/stable_diffusion/inference/gpu/main.py \
+    --save_image --channels_last
+
+else
+
+python -u ${MODEL_DIR}/models/generative-ai/pytorch/stable_diffusion/inference/gpu/main.py \
+    --save_image --channels_last --precision fp16
+fi
