4594a4fe978f465d68aa5af3d7310f1a032d3354
Srikanth Ramakrishna <srikanth.ramakrishna@intel.com>
2023-08-17 10:26:49 -0700

    itex atsm stable diffusion (#1418)
    * add stable diffusion scripts
    
    * add keras support
    
    * create stable diffusion quickstart scripts
    
    * change framework name
    
    * add precision input
    
    * add export and devcatalog
    
    * add store dir for inference
    
    * change folder names
    
    * make directory changes
    
    * rearrange files
    
    * change docker image name
    
    * change docker image name
    
    * remove reference dir
    
    * output dir correction
    
    * change paths
    
    * change docker name and remove dataset
    
    * remove store result dir
    
    * change precisions naming
    
    * rename docker image name and folder structure
    
    * remove data files
    
    * add license headers

---
+++ quickstart/generative-ai/tensorflow/stable_diffusion/inference/gpu/online_inference.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+MODEL_DIR=${MODEL_DIR-$PWD}
+
+echo 'MODEL_DIR='$MODEL_DIR
+echo 'OUTPUT_DIR='$OUTPUT_DIR
+echo 'PRECISION='$PRECISION
+
+# Create an array of input directories that are expected and then verify that they exist
+declare -A input_envs
+
+input_envs[OUTPUT_DIR]=${OUTPUT_DIR}
+
+for i in "${!input_envs[@]}"; do
+  var_name=$i
+  env_param=${input_envs[$i]}
+ 
+  if [[ -z $env_param ]]; then
+    echo "The required environment variable $var_name is not set" >&2
+    exit 1
+  fi
+done
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+# If batch size env is not mentioned, then the workload will run with the default batch size.
+if [ -z "${BATCH_SIZE}" ]; then
+  BATCH_SIZE="1"
+  echo "Running with default batch size of ${BATCH_SIZE}"
+fi
+
+python -u ${MODEL_DIR}/models/generative-ai/tensorflow/stable_diffusion/inference/gpu/stable_diffusion_inference.py --precision ${PRECISION} --store_result_dir ${OUTPUT_DIR}
