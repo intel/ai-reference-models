b02e1cf490cc5282a898f575d890f2ea8a5f0691
DiweiSun <105627594+DiweiSun@users.noreply.github.com>
2023-09-07 10:22:40 +0800

    Molly/accuracy llm (#1488)
    * llama and gpt-j accuracy extract
    
    * training results extract
    
    ---------
    
    Co-authored-by: zengxian <xiangdong.zeng@intel.com>

--- quickstart/language_modeling/pytorch/gptj/inference/cpu/run_accuracy.sh
+++ quickstart/language_modeling/pytorch/gptj/inference/cpu/run_accuracy.sh
@@ -77,7 +77,7 @@ FINETUNED_MODEL=${FINETUNED_MODEL:-"'EleutherAI/gpt-j-6b'"}
 EVAL_SCRIPT=${EVAL_SCRIPT:-"../../../../../../models/language_modeling/pytorch/gptj/inference/cpu/run_llm.py"}
 WORK_SPACE=${WORK_SPACE:-${OUTPUT_DIR}}
 rm -rf ${OUTPUT_DIR}/latency_log*
-python -m intel_extension_for_pytorch.cpu.launch --node_id 0 --enable_tcmalloc --log_path=${OUTPUT_DIR} --log_file_prefix="./latency_log_${precision}_${mode}" \
+python -m intel_extension_for_pytorch.cpu.launch --node_id 0 --enable_tcmalloc --log_path=${OUTPUT_DIR} --log_file_prefix="./GPT-J_${precision}_accuracy_${mode}" \
   ${EVAL_SCRIPT} $ARGS \
   --model-name-or-path   ${FINETUNED_MODEL} \
 
