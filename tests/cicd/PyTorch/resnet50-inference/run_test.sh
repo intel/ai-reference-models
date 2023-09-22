c1cde0a2d564a3a1f7083a56e5c413cd8fae5a4b
Wafaa Taie <wafaa.s.taie@intel.com>
2023-09-22 11:20:59 -0700

    Fix LKG pipeline for the new ipex and itex conda installers (#1525)
    * update LKG pipeline for the new ipex and itex conda installers
    
    * remove version subdir from bom file

--- tests/cicd/PyTorch/resnet50-inference/run_test.sh
+++ tests/cicd/PyTorch/resnet50-inference/run_test.sh
@@ -14,13 +14,14 @@ DATASET=$5
 mkdir -p ${OUTPUT_DIR}
 
 if [[ "${is_lkg_drop}" == "true" ]]; then
-  export PATH=${WORKSPACE}/miniconda3/bin:$PATH
-  source ${WORKSPACE}/pytorch_setup/setvars.sh
-  source ${WORKSPACE}/pytorch_setup/compiler/latest/env/vars.sh
-  source ${WORKSPACE}/pytorch_setup/mkl/latest/env/vars.sh
-  source ${WORKSPACE}/pytorch_setup/tbb/latest/env/vars.sh
-  source ${WORKSPACE}/pytorch_setup/mpi/latest/env/vars.sh
-  conda activate pytorch
+  #export PATH=${WORKSPACE}/miniconda3/bin:$PATH
+  #source ${WORKSPACE}/pytorch_setup/setvars.sh
+  #source ${WORKSPACE}/pytorch_setup/compiler/latest/env/vars.sh
+  #source ${WORKSPACE}/pytorch_setup/mkl/latest/env/vars.sh
+  #source ${WORKSPACE}/pytorch_setup/tbb/latest/env/vars.sh
+  #source ${WORKSPACE}/pytorch_setup/mpi/latest/env/vars.sh
+  source ${WORKSPACE}/pytorch_setup/bin/activate pytorch
+  #conda activate pytorch
 fi
 
 # Run script
--- tests/cicd/TensorFlow/mobilenetv1-inference/run_test.sh
+++ tests/cicd/TensorFlow/mobilenetv1-inference/run_test.sh
@@ -20,13 +20,13 @@ elif [[ "${PRECISION}" == "int8" ]]; then
 fi
 
 if [[ "${is_lkg_drop}" == "true" ]]; then
-  export PATH=${WORKSPACE}/miniconda3/bin:$PATH
-  source ${WORKSPACE}/tensorflow_setup/setvars.sh
-  source ${WORKSPACE}/tensorflow_setup/compiler/latest/env/vars.sh
-  source ${WORKSPACE}/tensorflow_setup/mkl/latest/env/vars.sh
-  source ${WORKSPACE}/tensorflow_setup/tbb/latest/env/vars.sh
-  source ${WORKSPACE}/tensorflow_setup/mpi/latest/env/vars.sh
-  conda activate tensorflow
+  #export PATH=${WORKSPACE}/miniconda3/bin:$PATH
+  #source ${WORKSPACE}/tensorflow_setup/setvars.sh
+  #source ${WORKSPACE}/tensorflow_setup/compiler/latest/env/vars.sh
+  #source ${WORKSPACE}/tensorflow_setup/mkl/latest/env/vars.sh
+  #source ${WORKSPACE}/tensorflow_setup/tbb/latest/env/vars.sh
+  #source ${WORKSPACE}/tensorflow_setup/mpi/latest/env/vars.sh
+  source ${WORKSPACE}/tensorflow_setup/bin/activate tensorflow
 fi
 
 # run following script
