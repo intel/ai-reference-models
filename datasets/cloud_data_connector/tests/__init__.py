a84ce7c86ac04ab783e33629602a10cab5b3809d
Jesus Herrera Ledon <110855758+j3su5pro-intel@users.noreply.github.com>
2023-12-11 15:49:18 -0600

    Remove MLFlow dependency. Updates on functional tests (#1756)

--- datasets/cloud_data_connector/tests/__init__.py
+++ datasets/cloud_data_connector/tests/__init__.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import sys 
 import cloud_data_connector.cloud_data_connector
 sys.modules['cloud_data_connector'] = cloud_data_connector.cloud_data_connector
+
