160606178f74dd44239667eacfa46bd8f93d5877
Om Thakkar <om.thakkar@intel.com>
2023-12-22 10:46:08 -0800

    Predownload weights for SDv2.1 (#1797)
    * predownload weights for SDv2.1
    
    * update hash

--- benchmarks/common/tensorflow/start.sh
+++ benchmarks/common/tensorflow/start.sh
@@ -1817,11 +1817,11 @@ function stable_diffusion() {
             "https://github.com/openai/CLIP/blob/main/clip/bpe_simple_vocab_16e6.txt.gz?raw=true",
             file_hash="924691ac288e54409236115652ad4aa250f48203de50a9e4722a6ecd48d6804a",
         )\n_ = keras.utils.get_file(
-          origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/kcv_encoder.h5",
-          file_hash="4789e63e07c0e54d6a34a29b45ce81ece27060c499a709d556c7755b42bb0dc4",
+          origin="https://huggingface.co/ianstenbit/keras-sd2.1/resolve/main/text_encoder_v2_1.h5",
+          file_hash="985002e68704e1c5c3549de332218e99c5b9b745db7171d5f31fcd9a6089f25b",
         )\n_ = keras.utils.get_file(
-          origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/kcv_diffusion_model.h5",
-          file_hash="8799ff9763de13d7f30a683d653018e114ed24a6a819667da4f5ee10f9e805fe",
+          origin="https://huggingface.co/ianstenbit/keras-sd2.1/resolve/main/diffusion_model_v2_1.h5",
+          file_hash="c31730e91111f98fe0e2dbde4475d381b5287ebb9672b1821796146a25c5132d",
         )\n_ = keras.utils.get_file(
           origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/kcv_decoder.h5",
           file_hash="ad350a65cc8bc4a80c8103367e039a3329b4231c2469a1093869a345f55b1962",
