0330fced31d592db309e6c0844aa041ed51d6c21
sachinmuradi <sachin.muradi@intel.com>
2023-12-28 12:00:45 -0800

    Keras 3 fix for ViT training (#1778)
    * Add keras 3 fix to transformers , advanced ops
    
    * force install transformer branch

--- quickstart/image_recognition/tensorflow/vision_transformer/training/cpu/run_vit_fine_tune_multiinstance.sh
+++ quickstart/image_recognition/tensorflow/vision_transformer/training/cpu/run_vit_fine_tune_multiinstance.sh
@@ -45,6 +45,11 @@ if [ ! -d "${INIT_CHECKPOINT_DIR}" ]; then
   exit 1
 fi
 
+if [[ ${TF_USE_LEGACY_KERAS} == "1" ]]; then
+        echo "Information: Installing transformers with keras fix...!"
+        echo "python3 -m pip install git+https://github.com/intel-tensorflow/transformers@vit-keras3-fix"
+        python3 -m pip install git+https://github.com/intel-tensorflow/transformers@vit-keras3-fix --force-reinstall
+fi
 
 # If precision env is not mentioned, then the workload will run with the default precision.
 if [ -z "${PRECISION}"]; then
@@ -56,6 +61,29 @@ if [[ $PRECISION != "fp32" ]] && [[ $PRECISION != "bfloat16" ]] && [[ $PRECISION
   echo "The specified precision '${PRECISION}' is unsupported."
   echo "Supported precision is float32 , bfloat16, float16."
   exit 1
+fi
+
+echo "Advanced settings for improved performance : "
+echo "Setting TF_USE_ADVANCED_CPU_OPS to 1, to enhace the overall performance"
+export TF_USE_ADVANCED_CPU_OPS=1;
+echo "TF_USE_ADVANCED_CPU_OPS = ${TF_USE_ADVANCED_CPU_OPS}"
+
+echo "Setting thread pinning and spinning"
+export TF_THREAD_PINNING_MODE=compact,39,400;
+echo "TF_THREAD_PINNING_MODE = ${TF_THREAD_PINNING_MODE}"
+
+if [[ ${TF_USE_ADVANCED_CPU_OPS} == "1" ]]; then
+	if [[ $PRECISION == "bfloat16" ]]; then
+		echo "TF_USE_ADVANCED_CPU_OPS is on for bfloat16 precision"
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD=Gelu,GeluGrad;
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD}"
+	elif [[ $PRECISION == "fp16" ]]; then
+		echo "TF_USE_ADVANCED_CPU_OPS is on for fp16 precision"
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD=Mean,Gelu,GeluGrad,Sum,Square;
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE=Mean,Sum;
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD}"
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE}"
+	fi
 fi
 
 # If steps env is not mentioned, then the workload will run with the default steps.
