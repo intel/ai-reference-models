37cb601f6b7502d1d7ce008074ddeb6ed4490a3e
Kanvi Khanna <kanvi.khanna@intel.com>
2024-01-03 11:55:35 -0800

    Update quickstart scripts with env vars (#1814)

--- quickstart/image_recognition/tensorflow/vision_transformer/inference/cpu/inference_realtime_multi_instance.sh
+++ quickstart/image_recognition/tensorflow/vision_transformer/inference/cpu/inference_realtime_multi_instance.sh
@@ -74,6 +74,31 @@ else
   WARMUP_STEPS="warmup_steps=$WARMUP_STEPS"
 fi
 echo "WARMUP_STEPS: $WARMUP_STEPS"
+echo "CORES_PER_INSTANCE: $CORES_PER_INSTANCE"
+
+# Setting environment variables
+echo "Advanced settings for improved performance : "
+echo "Setting TF_USE_ADVANCED_CPU_OPS to 1, to enhace the overall performance"
+export TF_USE_ADVANCED_CPU_OPS=1
+echo "TF_USE_ADVANCED_CPU_OPS = ${TF_USE_ADVANCED_CPU_OPS}"
+
+if [[ ${TF_USE_ADVANCED_CPU_OPS} == "1" ]]; then
+	if [[ $PRECISION == "bfloat16" ]]; then
+		echo "TF_USE_ADVANCED_CPU_OPS is on for bfloat16 precision"
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD=Gelu
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD}"
+	elif [[ $PRECISION == "fp16" ]]; then
+		echo "TF_USE_ADVANCED_CPU_OPS is on for fp16 precision"
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD=Mean,Gelu
+		export TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE=Mean
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_INFERLIST_ADD}"
+		echo "TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE = ${TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_DENYLIST_REMOVE}"
+	fi
+fi
+
+echo "Configuring thread pinning and spinning settings"
+export TF_THREAD_PINNING_MODE=none,$(($CORES_PER_INSTANCE-1)),400
+echo "TF_THREAD_PINNING_MODE: $TF_THREAD_PINNING_MODE"
 
 # Get number of cores per socket line from lscpu
 cores_per_socket=$(lscpu |grep 'Core(s) per socket:' |sed 's/[^0-9]//g')
