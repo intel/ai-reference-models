b15d2d0c9aa1571b50255fcaf1b966793a1f0008
wincent8 <wei.liao@intel.com>
2024-03-05 04:10:05 +0800

    add rnnt (#1703)
    * add rnnt
    
    Co-authored-by: Mahathi Vatsal <mahathi.vatsal.salopanthula@intel.com>
    Co-authored-by: Srikanth Ramakrishna <srikanth.ramakrishna@intel.com>

---
+++ models_v2/pytorch/rnnt/inference/gpu/loss.py
@@ -0,0 +1,159 @@
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2019, Myrtle Software Limited. All rights reserved.
+# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+from typing import Tuple
+
+import torch
+# from warprnnt_pytorch import RNNTLoss as WarpRNNTLoss
+
+
+class RNNTLoss(torch.nn.Module):
+    """Wrapped :py:class:`warprnnt_pytorch.RNNTLoss`.
+    Args:
+        blank: Index of the blank label.
+        reduction: (string) Specifies the reduction to apply to the output:
+            none:
+                No reduction will be applied.
+            mean:
+                The output losses will be divided by the target lengths and
+                then the mean over the batch is taken.
+            sum:
+                Sum all losses in a batch.
+    Attributes:
+        rnnt_loss: A :py:class:`warprnnt_pytorch.RNNTLoss` instance.
+    """
+
+    def __init__(self, blank, reduction="mean"):
+        super().__init__()
+        self.rnnt_loss = WarpRNNTLoss(blank=blank)
+        #self.use_cuda = torch.cuda.is_available()
+        self.use_cuda = False
+
+    def forward(
+        self,
+        args,
+        inputs: Tuple[torch.Tensor, torch.Tensor],
+        targets: Tuple[torch.Tensor, torch.Tensor],
+    ) -> torch.Tensor:
+        """Computes RNNT loss.
+        All inputs are moved to the GPU with :py:meth:`torch.nn.Module.cuda` if
+        :py:func:`torch.cuda.is_available` was :py:data:`True` on
+        initialisation.
+        Args:
+            inputs: A tuple where the first element is the unnormalized network
+                :py:class:`torch.Tensor` outputs of size ``[batch, max_seq_len,
+                max_output_seq_len + 1, vocab_size + 1)``. The second element
+                is a Tuple of two :py:class:`torch.Tensor`s both of
+                size ``[batch]`` that contain the lengths of a) the audio features
+                logits and b) the target sequence logits.
+            targets: A tuple where the first element is a
+                :py:class:`torch.Tensor` such that each entry in the target
+                sequence is a class index. Target indices cannot be the blank
+                index. It must have size ``[batch, max_seq_len]``. In the former
+                form each target sequence is padded to the length of the longest
+                sequence and stacked.
+                The second element is a :py:class:`torch.Tensor` that gives
+                the lengths of the targets. Lengths are specified for each
+                sequence to achieve masking under the assumption that sequences
+                are padded to equal lengths.
+        """
+
+        logits, logit_lens = inputs
+        y, y_lens = targets
+
+        # cast to required types
+        if logits.dtype != torch.float:
+            logits_orig = logits
+            logits = logits.float()
+            del logits_orig  # save memory *before* computing the loss
+
+        if y.dtype != torch.int32:
+            y = y.int()
+
+        if logit_lens.dtype != torch.int32:
+            logit_lens = logit_lens.int()
+
+        if y_lens.dtype != torch.int32:
+            y_lens = y_lens.int()
+
+        # send to gpu
+        if self.use_cuda:
+            logits = logits.cuda()
+            logit_lens = logit_lens.cuda()
+            y = y.cuda()
+            y_lens = y_lens.cuda()
+
+        # work-around
+        # rnnt_loss get loss from extension warp-rnnt
+        # and it cannot process xpu input, even in ut case
+        # need further investigation
+        logits = logits.to("cpu")
+        logit_lens = logit_lens.to("cpu")
+        y = y.to("cpu")
+        y_lens = y_lens.to("cpu")
+
+        loss = self.rnnt_loss(
+            acts=logits, labels=y, act_lens=logit_lens, label_lens=y_lens
+        )
+
+        loss = loss.to(args.xpu_id)
+        # del new variables that may have been created due to float/int/cuda()
+        del logits, y, logit_lens, y_lens, inputs, targets
+
+        return loss
+
+class TransducerLoss(torch.nn.Module):
+    def __init__(self, blank_idx, precision):
+        super().__init__()
+        self.t_loss = torch.xpu.TransducerLoss()
+        self.blank_idx = blank_idx
+        self.bf16 = precision
+
+    def forward(self, logits, logit_lens, y, y_lens, dict_meta_data=None):
+        if self.bf16 and logits.dtype != torch.float32:
+            logits = logits.float()
+
+        if y.dtype != torch.int32:
+            y = y.int()
+
+        if logit_lens.dtype != torch.int32:
+            logit_lens = logit_lens.int()
+
+        if y_lens.dtype != torch.int32:
+            y_lens = y_lens.int()
+        loss_grad = torch.ones(logits.size(0), dtype=logits.dtype, device=logits.device)/logits.size(0)
+
+        # loss = transducer_loss_reference(logits, y, logit_lens, y_lens, self.blank_idx, loss_grad
+        #                     ).mean()
+        loss = self.t_loss(logits, y, logit_lens, y_lens, self.blank_idx).mean()
+
+        return loss
+
---
+++ models_v2/pytorch/rnnt/training/gpu/CONTAINER.md
@@ -0,0 +1,71 @@
+# Running RNN-T Training on Intel® Data Center GPU Max Series using Intel® Extension for PyTorch*
+
+## Description 
+This document has instructions for running RNN-T Training using Intel® Extension for PyTorch on Intel®Max Series GPU. 
+
+## Requirements
+| Item | Detail |
+| ------ | ------- |
+| Host machine  | [Intel® Data Center GPU Max Series](https://ark.intel.com/content/www/us/en/ark/products/series/232874/intel-data-center-gpu-max-series.html)  |
+| Drivers | GPU-compatible drivers need to be installed: Download [Driver](https://dgpu-docs.intel.com/driver/installation.html) |
+| Software | Docker* |
+
+## Quick Start Scripts
+
+| Script name | Description |
+|-------------|-------------|
+| `run_model.sh` | Runs BF16,TF32 and FP32 training on single tile and two tiles|
+
+## Datasets and Pre-trained Models
+
+Refer to instructions [here](./README.md#prepare-dataset) to download and pre-process LibriSpeech dataset.Set the `DATASET_DIR` environment variable to point to the dataset directory respectively. 
+
+### Docker pull command:
+
+```
+docker pull intel/speech-recognition:pytorch-max-gpu-rnnt-training
+```
+The RNNT training container includes scripts, model and libraries needed to run BF16,TF32 and FP32 training. To run the `run_model.sh` quickstart script using this container, you'll need to set the environment variable and provide volume mounts for the Dataset and Pre-trained model. You will need to provide an output directory where log files will be written. 
+
+```bash
+#Optional
+export PRECISION=<provide either BF16,TF32 or FP32, otherwise (default: BF16)>
+export BATCH_SIZE=<provide batch size, otherwise (default: 512)>
+
+#Required
+export DATASET_DIR=<provide path to processed dataset>
+export PLATFORM=Max
+export MULTI_TILE=<provide True of False to enable/disable multi-tile training>
+export OUTPUT_DIR=<provide path to output logs directory>
+
+IMAGE_NAME=intel/speech-recognition:pytorch-max-gpu-rnnt-training
+DOCKER_ARGS="--rm -it"
+SCRIPT=run_model.sh
+
+docker run \
+  --device=/dev/dri \
+  --env BATCH_SIZE=${BATCH_SIZE} \
+  --env DATASET_DIR=${DATASET_DIR} \
+  --env PRECISION=${PRECISION} \
+  --env PLATFORM=${PLATFORM} \
+  --env OUTPUT_DIR=${OUTPUT_DIR} \
+  --env MULTI_TILE=${MULTI_TILE} \
+  --env http_proxy=${http_proxy} \
+  --env https_proxy=${https_proxy} \
+  --env no_proxy=${no_proxy} \
+  --volume ${DATASET_DIR}:${DATASET_DIR} \
+  --volume ${OUTPUT_DIR}:${OUTPUT_DIR} \
+  ${DOCKER_ARGS} \
+  $IMAGE_NAME \
+  /bin/bash $SCRIPT
+```
+## Documentation and Sources
+
+[GitHub* Repository](https://github.com/IntelAI/models/tree/master/docker/max-gpu)
+
+## Support
+Support for Intel® Extension for PyTorch* is found via the [Intel® AI Analytics Toolkit.](https://www.intel.com/content/www/us/en/developer/tools/oneapi/ai-analytics-toolkit.html#gs.qbretz) Additionally, the Intel® Extension for PyTorch* team tracks both bugs and enhancement requests using [GitHub issues](https://github.com/intel/intel-extension-for-pytorch/issues). Before submitting a suggestion or bug report, please search the GitHub issues to see if your issue has already been reported.
+
+## License Agreement
+
+LEGAL NOTICE: By accessing, downloading or using this software and any required dependent software (the “Software Package”), you agree to the terms and conditions of the software license agreements for the Software Package, which may also include notices, disclaimers, or license terms for third party software included with the Software Package. Please refer to the [license file](https://github.com/IntelAI/models/tree/master/third_party) for additional details.
