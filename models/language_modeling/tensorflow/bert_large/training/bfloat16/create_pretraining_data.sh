bc257ec34ab79557179d82a316fa6a177295424d
jojivk-intel-nervana <jojimon.varghese@intel.com>
2024-03-06 14:23:03 -0800

    Cleanup scripts in bert large dir. (#1920)
    * Fix names and remove scripts

--- models/language_modeling/tensorflow/bert_large/training/bfloat16/create_pretraining_data.sh
+++ models/language_modeling/tensorflow/bert_large/training/bfloat16/create_pretraining_data.sh
@@ -1,12 +1,28 @@
+
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 date
-export BERT_BASE_DIR=/nfs/site/home/jojimonv/Tensorflow/Models/Intel/BERT/uncased_L-12_H-768_A-12
-export GLUE_DIR=/nfs/site/home/jojimonv/Tensorflow/Models/Intel/BERT/glue/glue_data
+if [ -z "$BERT_BASE_DIR" ]; then 
+  echo "ERROR: empty BERT_BASE_DIR" 
+fi
+if [ -z "$GLUE_DIR" ]; then 
+  echo "ERROR: empty GLUE_DIR" 
+fi
 export TF_CPP_MIN_VLOG_LEVEL=0
 export MKL_DNN_VERBOSE=0
-
-echo "====================================================="
-echo "    Running create_pretraining.py for $1...."
-echo "====================================================="
 python create_pretraining_data.py \
   --input_file=./sample_text.txt \
   --output_file=./output/tf_examples.tfrecord \
