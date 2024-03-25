0851a10b7e151f0a57a9d3076be40176e01af2db
Nick Camarena <91098672+nickcama@users.noreply.github.com>
2024-03-25 10:52:01 -0700

    [Tensorflow] Enable bfloat16, fp16, and int8 for Yolo V5 (#1790)
    * enable yolov5 on CPU
    
    Co-authored-by: nick.camarena <nick.camarena@intel.com>
    Co-authored-by: Clayne Robison <clayne.b.robison@intel.com>
    Co-authored-by: Jitendra Patil <jitendra.patil@intel.com>

--- quickstart/object_detection/tensorflow/yolov5/inference/cpu/inference.sh
+++ quickstart/object_detection/tensorflow/yolov5/inference/cpu/inference.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 #
+# -*- coding: utf-8 -*-
+#
 # Copyright (c) 2023 Intel Corporation
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# AGPL-3.0 license
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 
 MODEL_DIR=${MODEL_DIR-$PWD}
 
@@ -37,16 +30,15 @@ fi
 
 if [ -z "${PRECISION}" ]; then
   echo "The required environment variable PRECISION has not been set"
-  echo "Please set PRECISION to fp32 or bfloat16"
+  echo "Please set PRECISION to fp32, bfloat16, fp16, int8"
   exit 1
 fi
 
-if [[ $PRECISION != "fp32" ]] || [ ${PRECISION} == "bfloat16" ]; then
+if [[ $PRECISION != "fp32" ]] && [[ $PRECISION != "int8" ]] && [[ $PRECISION != "bfloat16" ]] && [[ $PRECISION != "fp16" ]]; then
   echo "The specified precision '${PRECISION}' is unsupported."
-  echo "Supported precisions are: fp32 and bfloat16"
+  echo "Supported precisions are: fp32, bfloat16, fp16 and int8"
   exit 1
 fi
-
 
 if [[ ! -f "${PRETRAINED_MODEL}" ]]; then
   echo "The file specified by the PRETRAINED_MODEL environment variable (${PRETRAINED_MODEL}) does not exist."
@@ -57,6 +49,11 @@ fi
 if [ -z "${BATCH_SIZE}" ]; then
   BATCH_SIZE="1"
   echo "Running with default batch size of ${BATCH_SIZE}"
+fi
+
+if [[ $PRECISION == "fp16" ]]; then
+  export ONEDNN_MAX_CPU_ISA=AVX512_CORE_AMX_FP16
+  echo "ONEDNN_MAX_CPU_ISA=$ONEDNN_MAX_CPU_ISA"
 fi
 
 source "${MODEL_DIR}/quickstart/common/utils.sh"
@@ -70,5 +67,6 @@ _command python benchmarks/launch_benchmark.py \
          --output-dir ${OUTPUT_DIR} \
          --batch-size=${BATCH_SIZE} \
          --socket-id 0 \
+         --benchmark-only \
          $@ \
          $ARGS
