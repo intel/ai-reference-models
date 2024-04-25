e7f2b07f49689614cccd784a27be5051e51243bc
Tanner Voas <62364327+tannervoas742@users.noreply.github.com>
2024-04-25 08:17:13 +0800

    ipex: minor fixes for benchmark.sh in various samples
    - !bin/bash needs to be at top of file
    
    Signed-off-by: Voas, Tanner <tanner.voas@intel.com>

--- models_v2/pytorch/efficientnet/inference/gpu/benchmark.sh
+++ models_v2/pytorch/efficientnet/inference/gpu/benchmark.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright (c) 2023-2024 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
 
 (( EUID != 0 )) && echo "fatal: must be executed under root" && exit 1
 
