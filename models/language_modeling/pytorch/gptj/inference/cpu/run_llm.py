f308a0174323173cb9ad6b8f70b13a8304ff5c50
jianan-gu <jianan.gu@intel.com>
2024-04-26 15:43:55 +0800

    enable llama/gptj/chatglm for llm accuracy with inductor path (#2153)

--- models/language_modeling/pytorch/gptj/inference/cpu/run_llm.py
+++ models/language_modeling/pytorch/gptj/inference/cpu/run_llm.py
@@ -113,29 +113,32 @@ if args.ipex:
         prepare_fp8,
     )
 
+
 def trace_handler(prof):
-    print(prof.key_averages().table(
-        sort_by="self_cpu_time_total", row_limit=10))
+    print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10))
     import datetime
+
     now = datetime.datetime.now()
-    log_path = os.path.join(os.getcwd(), "gptj_profiling_{}_step_{}.json".format(now.strftime("%Y%m%d%H%M%S"), str(prof.step_num)))
+    log_path = os.path.join(
+        os.getcwd(),
+        "gptj_profiling_{}_step_{}.json".format(
+            now.strftime("%Y%m%d%H%M%S"), str(prof.step_num)
+        ),
+    )
     prof.export_chrome_trace(log_path)
+
 
 profile_ctx = torch.profiler.profile(
     activities=[
         torch.profiler.ProfilerActivity.CPU,
     ],
-    schedule=torch.profiler.schedule(
-        wait=0,
-        warmup=0,
-        active=1,
-        repeat=1),
+    schedule=torch.profiler.schedule(wait=0, warmup=0, active=1, repeat=1),
     on_trace_ready=trace_handler,
     record_shapes=True,
     profile_memory=True,
     with_stack=True,
     with_flops=True,
-    with_modules=True
+    with_modules=True,
 )
 
 # beam search = 4
@@ -168,14 +171,18 @@ else:
 has_position_id = False
 if "llama" in args.model_name_or_path:
     user_model = LlamaForCausalLM.from_pretrained(
-        args.model_name_or_path, low_cpu_mem_usage=True, torchscript=args.jit,
+        args.model_name_or_path,
+        low_cpu_mem_usage=True,
+        torchscript=args.jit,
     )
     tokenizer = LlamaTokenizer.from_pretrained(args.model_name_or_path)
     if hasattr(user_model.config, "num_key_value_heads"):
         del user_model.config.num_key_value_heads
 else:
     user_model = AutoModelForCausalLM.from_pretrained(
-        args.model_name_or_path, low_cpu_mem_usage=True, torchscript=args.jit,
+        args.model_name_or_path,
+        low_cpu_mem_usage=True,
+        torchscript=args.jit,
     )
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 get_memory_usage("Host", args)
@@ -190,14 +197,19 @@ if args.dtype == "bf16" or args.dtype == "fp32":
         )
     if args.inductor:
         from torch._inductor import config as inductor_config
+
         inductor_config.cpp_wrapper = True
-        with torch.no_grad(), torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
+        with torch.no_grad(), torch.cpu.amp.autocast(
+            enabled=amp_enabled, dtype=amp_dtype
+        ):
             if args.ipex:
-                print('[Info] Running torch.compile() with IPEX backend')
-                user_model = torch.compile(user_model, backend="ipex")
+                print("[Info] Running torch.compile() with IPEX backend")
+                user_model.forward = torch.compile(
+                    user_model.forward, dynamic=True, backend="ipex"
+                )
             else:
-                print('[Info] Running torch.compile() BFloat16 with default backend')
-                user_model.forward = torch.compile(user_model.forward)
+                print("[Info] Running torch.compile() BFloat16 with default backend")
+                user_model.forward = torch.compile(user_model.forward, dynamic=True)
 elif args.dtype == "fp16":
     if args.ipex:
         user_model = ipex.llm.optimize(
@@ -208,14 +220,19 @@ elif args.dtype == "fp16":
         )
     if args.inductor:
         from torch._inductor import config as inductor_config
+
         inductor_config.cpp_wrapper = True
-        with torch.no_grad(), torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
+        with torch.no_grad(), torch.cpu.amp.autocast(
+            enabled=amp_enabled, dtype=amp_dtype
+        ):
             if args.ipex:
-                print('[Info] Running torch.compile() with IPEX backend')
-                user_model = torch.compile(user_model, backend="ipex")
+                print("[Info] Running torch.compile() with IPEX backend")
+                user_model.forward = torch.compile(
+                    user_model.forward, dynamic=True, backend="ipex"
+                )
             else:
-                print('[Info] Running torch.compile() BFloat16 with default backend')
-                user_model.forward = torch.compile(user_model.forward)
+                print("[Info] Running torch.compile() BFloat16 with default backend")
+                user_model.forward = torch.compile(user_model.forward, dynamic=True)
 elif args.dtype == "bf32":
     ipex.set_fp32_math_mode(mode=ipex.FP32MathMode.BF32, device="cpu")
     user_model = ipex.llm.optimize(
@@ -300,7 +317,6 @@ elif re.search("llama", user_model.config.architectures[0], re.IGNORECASE):
     ]
 
 
-
 if global_past_key_value == None:
     print("This scirpt only supports llama gptj.")
     exit(0)
@@ -346,26 +362,29 @@ class Evaluator:
             attention_mask_padded.append(attention_mask)
             position_ids = pad(position_ids, (0, pad_len), value=self.pad_val)
             position_ids_padded.append(position_ids)
-        if has_position_id:
-            return (
-                (
-                    torch.vstack(input_ids_padded),
-                    torch.vstack(attention_mask_padded),
-                    tuple(global_past_key_value),
-                    torch.vstack(position_ids_padded),
-                ),
-                torch.tensor(last_ind),
-            )
+        if args.inductor:
+            return ((torch.vstack(input_ids_padded)), torch.tensor(last_ind))
         else:
-            return (
-                (
-                    torch.vstack(input_ids_padded),
-                    torch.vstack(attention_mask_padded),
-                    tuple(global_past_key_value),
-                    None,
-                ),
-                torch.tensor(last_ind),
-            )
+            if has_position_id:
+                return (
+                    (
+                        torch.vstack(input_ids_padded),
+                        torch.vstack(attention_mask_padded),
+                        tuple(global_past_key_value),
+                        torch.vstack(position_ids_padded),
+                    ),
+                    torch.tensor(last_ind),
+                )
+            else:
+                return (
+                    (
+                        torch.vstack(input_ids_padded),
+                        torch.vstack(attention_mask_padded),
+                        tuple(global_past_key_value),
+                        None,
+                    ),
+                    torch.tensor(last_ind),
+                )
 
     @torch.no_grad()
     def evaluate(self, model):
@@ -379,57 +398,87 @@ class Evaluator:
             collate_fn=self.collate_batch,
         )
 
-        for i, (
-            (input_ids, attention_mask, past_key_values, position_ids),
-            last_ind,
-        ) in enumerate(test_dataloader):
-            label = input_ids[torch.arange(len(last_ind)), last_ind]
-            input_ids[torch.arange(len(last_ind)), last_ind] = self.pad_val
-            pad_len = self.pad_max - last_ind - 1
-            start = time.time()
-            if has_position_id:
-                if args.dtype == "fp8":
-                    outputs = model(
-                        input_ids,
-                        attention_mask=attention_mask,
-                        position_ids=position_ids,
-                        # fp8 does not use llm.optimize, don't use past_key_values
-                    )
+        if args.inductor:
+            for i, ((input_ids), last_ind) in enumerate(test_dataloader):
+                label = input_ids[torch.arange(len(last_ind)), last_ind]
+                input_ids[torch.arange(len(last_ind)), last_ind] = self.pad_val
+                pad_len = self.pad_max - last_ind - 1
+                start = time.time()
+                outputs = model(
+                    input_ids,
+                )
+                latency += time.time() - start
+                if isinstance(outputs, tuple):
+                    res = outputs[0]
                 else:
-                    outputs = model(
-                        input_ids,
-                        attention_mask=attention_mask,
-                        position_ids=position_ids,
-                        past_key_values=past_key_values,
-                    )
-            else:
-                if args.dtype == "fp8":
-                    outputs = model(
-                        input_ids,
-                        attention_mask=attention_mask,
-                    )
+                    res = outputs["logits"]
+                last_token_logits = res[torch.arange(len(last_ind)), -2 - pad_len, :]
+
+                pred = last_token_logits.argmax(dim=-1)
+                total += label.size(0)
+                hit += (pred == label).sum().item()
+                if i % 50 == 0:
+                    print(hit / total)
+                    print("Processed minibatch:", i)
+
+            acc = hit / total
+            print(acc)
+            lantecy = latency / len(self.dataset)
+            return acc, lantecy
+        else:
+            for i, (
+                (input_ids, attention_mask, past_key_values, position_ids),
+                last_ind,
+            ) in enumerate(test_dataloader):
+                label = input_ids[torch.arange(len(last_ind)), last_ind]
+                input_ids[torch.arange(len(last_ind)), last_ind] = self.pad_val
+                pad_len = self.pad_max - last_ind - 1
+                start = time.time()
+                if has_position_id:
+                    if args.dtype == "fp8":
+                        outputs = model(
+                            input_ids,
+                            attention_mask=attention_mask,
+                            position_ids=position_ids,
+                            # fp8 does not use llm.optimize, don't use past_key_values
+                        )
+                    else:
+                        outputs = model(
+                            input_ids,
+                            attention_mask=attention_mask,
+                            position_ids=position_ids,
+                            past_key_values=past_key_values,
+                        )
                 else:
-                    outputs = model(
-                        input_ids,
-                        attention_mask=attention_mask,
-                        past_key_values=past_key_values,
-                    )
+                    if args.dtype == "fp8":
+                        outputs = model(
+                            input_ids,
+                            attention_mask=attention_mask,
+                        )
+                    else:
+                        outputs = model(
+                            input_ids,
+                            attention_mask=attention_mask,
+                            past_key_values=past_key_values,
+                        )
 
-            latency += time.time() - start
+                latency += time.time() - start
 
-            last_token_logits = outputs[0][torch.arange(len(last_ind)), -2 - pad_len, :]
+                last_token_logits = outputs[0][
+                    torch.arange(len(last_ind)), -2 - pad_len, :
+                ]
 
-            pred = last_token_logits.argmax(dim=-1)
-            total += label.size(0)
-            hit += (pred == label).sum().item()
-            if i % 50 == 0:
-                print(hit / total)
-                print("Processed minibatch:", i)
+                pred = last_token_logits.argmax(dim=-1)
+                total += label.size(0)
+                hit += (pred == label).sum().item()
+                if i % 50 == 0:
+                    print(hit / total)
+                    print("Processed minibatch:", i)
 
-        acc = hit / total
-        print(acc)
-        lantecy = latency / len(self.dataset)
-        return acc, lantecy
+            acc = hit / total
+            print(acc)
+            lantecy = latency / len(self.dataset)
+            return acc, lantecy
 
 
 if args.lambada:
@@ -479,11 +528,12 @@ def calib_func(prepared_model):
 
 
 def eval_func(traced_model):
-    print('Evaluating LLM')
+    print("Evaluating LLM")
     acc, latency = evaluator.evaluate(traced_model)
     print("Accuracy:", acc)
     print("Latency (sec):", latency)
     return acc
+
 
 # input prompt
 current_path = pathlib.Path(__file__).parent.resolve()
@@ -569,12 +619,16 @@ if args.dtype == "int8" and args.ipex:
         print("model quantization - Done!")
 elif args.dtype == "int8" and args.inductor:
     from torch._inductor import config as inductor_config
+
     inductor_config.cpp_wrapper = True
     from torch.ao.quantization.quantize_pt2e import prepare_pt2e, convert_pt2e
     import torch.ao.quantization.quantizer.x86_inductor_quantizer as xiq
-    from torch.ao.quantization.quantizer.x86_inductor_quantizer import X86InductorQuantizer
+    from torch.ao.quantization.quantizer.x86_inductor_quantizer import (
+        X86InductorQuantizer,
+    )
     from torch._export import capture_pre_autograd_graph, dynamic_dim
-    print('[Info] Running torch.compile() INT8 quantization')
+
+    print("[Info] Running torch.compile() INT8 quantization")
     with torch.no_grad():
         encoded_input = tokenizer(prompt, return_tensors="pt")
         print("encoded_input is: {}".format(encoded_input), flush=True)
@@ -591,11 +645,13 @@ elif args.dtype == "int8" and args.inductor:
         torch.ao.quantization.move_exported_model_to_eval(converted_model)
         with torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
             if args.ipex:
-                print('[Info] Running torch.compile() with IPEX backend')
-                user_model = torch.compile(converted_model, backend="ipex")
+                print("[Info] Running torch.compile() with IPEX backend")
+                user_model = torch.compile(
+                    converted_model, dynamic=True, backend="ipex"
+                )
             else:
-                print('[Info] Running torch.compile() with default backend')
-                user_model = torch.compile(converted_model)
+                print("[Info] Running torch.compile() with default backend")
+                user_model = torch.compile(converted_model, dynamic=True)
             user_model(**encoded_input)
             user_model(**encoded_input)
 elif args.dtype == "fp8":
@@ -616,7 +672,11 @@ elif args.dtype == "fp8":
 
         prepared_model = prepare_fp8(user_model)
         prepared_model.eval()
-        with fp8_autocast(enabled=False, calibrating=True, fp8_recipe=DelayedScaling(fp8_format=Format.E4M3, amax_history_len=1024)):
+        with fp8_autocast(
+            enabled=False,
+            calibrating=True,
+            fp8_recipe=DelayedScaling(fp8_format=Format.E4M3, amax_history_len=1024),
+        ):
             with torch.no_grad():
                 for i, (
                     (input_ids, attention_mask, past_key_values, position_ids),
@@ -638,15 +698,18 @@ elif args.dtype == "fp8":
             torch.save(prepared_model.state_dict(), args.fp8_config)
             print("calibration Done!")
     else:
-        user_model= prepare_fp8(user_model)
+        user_model = prepare_fp8(user_model)
         if os.path.exists(args.fp8_config):
             user_model.load_state_dict(torch.load(args.fp8_config))
         print("model quantization - Done!")
 
+
 def benchmark_warmup(prompt):
     # start
     if args.profile:
-        with profile_ctx as prof, torch.inference_mode(), torch.no_grad(), torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
+        with profile_ctx as prof, torch.inference_mode(), torch.no_grad(), torch.cpu.amp.autocast(
+            enabled=amp_enabled, dtype=amp_dtype
+        ):
             for i in range(5):
                 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
                 output = user_model.generate(
@@ -723,45 +786,46 @@ if args.benchmark:
         benchmark_evaluate(prompt)
 
 if args.accuracy_only:
-    if args.dtype == "int8" or args.int8_bf16_mixed:
-        user_model = user_model.trace_graph
+    if not args.inductor:
+        if args.dtype == "int8" or args.int8_bf16_mixed:
+            user_model = user_model.trace_graph
 
-    if args.jit and (
-        args.dtype == "bf16"
-        or args.dtype == "fp32"
-        or args.dtype == "bf32"
-        or args.dtype == "fp16"
-    ):
-        input_ids = torch.ones(32).to(torch.long)
-        attention_mask = torch.ones(len(input_ids))
-        position_ids = torch.arange(len(input_ids))
-        last_ind = input_ids.shape[0] - 1
-        example_inputs = (
-            {
-                "input_ids": input_ids.unsqueeze(0),
-                "attention_mask": attention_mask.unsqueeze(0),
-                "position_ids": position_ids.unsqueeze(0),
-                "past_key_values": tuple(global_past_key_value),
-            }
-            if has_position_id
-            else {
-                "input_ids": input_ids.unsqueeze(0),
-                "attention_mask": attention_mask.unsqueeze(0),
-                "past_key_values": tuple(global_past_key_value),
-            }
-        )
-        with torch.no_grad(), torch.cpu.amp.autocast(
-            enabled=amp_enabled, dtype=amp_dtype
+        if args.jit and (
+            args.dtype == "bf16"
+            or args.dtype == "fp32"
+            or args.dtype == "bf32"
+            or args.dtype == "fp16"
         ):
-            user_model = torch.jit.trace(
-                user_model.eval(),
-                example_kwarg_inputs=example_inputs,
-                strict=False,
-                check_trace=False,
+            input_ids = torch.ones(32).to(torch.long)
+            attention_mask = torch.ones(len(input_ids))
+            position_ids = torch.arange(len(input_ids))
+            last_ind = input_ids.shape[0] - 1
+            example_inputs = (
+                {
+                    "input_ids": input_ids.unsqueeze(0),
+                    "attention_mask": attention_mask.unsqueeze(0),
+                    "position_ids": position_ids.unsqueeze(0),
+                    "past_key_values": tuple(global_past_key_value),
+                }
+                if has_position_id
+                else {
+                    "input_ids": input_ids.unsqueeze(0),
+                    "attention_mask": attention_mask.unsqueeze(0),
+                    "past_key_values": tuple(global_past_key_value),
+                }
             )
-            user_model = torch.jit.freeze(user_model.eval())
+            with torch.no_grad(), torch.cpu.amp.autocast(
+                enabled=amp_enabled, dtype=amp_dtype
+            ):
+                user_model = torch.jit.trace(
+                    user_model.eval(),
+                    example_kwarg_inputs=example_inputs,
+                    strict=False,
+                    check_trace=False,
+                )
+                user_model = torch.jit.freeze(user_model.eval())
 
-    if args.dtype == "fp8":
+    if args.dtype == "fp8" and not args.inductor:
         input_ids = torch.ones(32).to(torch.long)
         attention_mask = torch.ones(len(input_ids))
         position_ids = torch.arange(len(input_ids))
@@ -781,7 +845,11 @@ if args.accuracy_only:
             }
         )
 
-        with fp8_autocast(enabled=True, calibrating=False, fp8_recipe=DelayedScaling(fp8_format=Format.E4M3, amax_history_len=1024)):
+        with fp8_autocast(
+            enabled=True,
+            calibrating=False,
+            fp8_recipe=DelayedScaling(fp8_format=Format.E4M3, amax_history_len=1024),
+        ):
             eval_func(user_model)
     else:
         with torch.cpu.amp.autocast(enabled=amp_enabled, dtype=amp_dtype):
