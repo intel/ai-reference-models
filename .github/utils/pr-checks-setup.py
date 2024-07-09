2f49f39f4d9eec8c1abbb24dab690d3bd4a5b58c
luisreal <112975902+lerealno@users.noreply.github.com>
2024-07-09 15:00:44 -0600

    Add script to comment on BoM change (#2323)
    * Add comment logic for BoM changes
    
    * Add script to post and update bom change comments
    
    * Add bom comment to pr checks workflow
    
    * Fix linting erros

--- .github/utils/pr-checks-setup.py
+++ .github/utils/pr-checks-setup.py
@@ -4,6 +4,7 @@ import re
 import json
 import argparse
 import requests
+import hashlib
 
 def main(args):
     with open(args.config_file, "r") as config_file:
@@ -15,9 +16,13 @@ def main(args):
         "X-GitHub-Api-Version": "2022-11-28",
        'Accept': 'application/vnd.github+json' }
     pr_info = json.loads(requests.get(url, headers=heads).text)
+    
+    print(json.dumps(build_checks_json(pr_info, config, url)))
 
+
+def build_checks_json(pr_info, config, api_url):
     # structure to store the checks to run and related information
-    checks = {
+    checks_json = {
         "flags": {
             "is_bom_change": False,
             "is_model_change": False,
@@ -26,49 +31,68 @@ def main(args):
         "files": {"pr": [], "bom": []},
         "dirs": {"workloads_to_run": set(), "containers_to_check": set()},
     }
-    # directory structure for model or container dirs
-    pipe = "|"
+    
+    PIPE = "|"
+
+    url = api_url.replace('api.', '').replace('repos/', '').replace('pulls', 'pull')
+
+    # directory structure for models dir
     valid_model_dir = re.compile(
         "^models_v2/"
-        + f'({pipe.join(config["frameworks"])})/[\w-]+/({pipe.join(config["mode"])})/({pipe.join(config["platform"])})/[\w/.-]+$'
-    )
-    valid_container_dir = re.compile(
-        "^docker/"
-        + f'({pipe.join(config["frameworks"])})/[\w-]+/({pipe.join(config["mode"])})/({pipe.join(config["platform"])})/[\w/.-]+$'
+        + f'({PIPE.join(config["frameworks"])})/[\w-]+/({PIPE.join(config["mode"])})/({PIPE.join(config["platform"])})/[\w/.-]+$'
     )
 
+    # directory structure for containers dir
+    valid_container_dir = re.compile(
+        "^docker/"
+        + f'({PIPE.join(config["frameworks"])})/[\w-]+/({PIPE.join(config["mode"])})/({PIPE.join(config["platform"])})/[\w/.-]+$'
+    )
+
+    # review changed files in the PR and activate check to run accordingly 
     for file in pr_info[:]:
         if file["status"] in ["added", "modified", "renamed", "copied", "changed"]:
-            checks["files"]["pr"].append(f'{file["filename"]} [{file["status"]}]')
-            # BoM changes
+            checks_json["files"]["pr"].append(f'{file["filename"]} [{file["status"]}]')
+            # bom change
             if (
                 file["filename"].split("/")[0] in ["models_v2", "docker"]
                 and "requirements.txt" in file["filename"].split("/")[:]
             ):
-                checks["flags"]["is_bom_change"] = True
-                checks["files"]["bom"].append(file["filename"])
-            # Model change
+                checks_json["flags"]["is_bom_change"] = True
+                checks_json["files"]["bom"].append(build_filename_diff_url(file["filename"], url))
+            # mmodel change
             if valid_model_dir.match(file["filename"]):
                 model_root = "/".join(file["filename"].split("/")[0:5])
                 container_root = "/".join(file["filename"].split("/")[0:5]).replace(
                     "models_v2", "docker"
                 )
-                checks["flags"]["is_model_change"] = True
-                checks["dirs"]["workloads_to_run"].add(model_root)
-                # Add check for dependent containers
+                checks_json["flags"]["is_model_change"] = True
+                checks_json["dirs"]["workloads_to_run"].add(model_root)
+                # add check for dependent containers
                 if os.path.exists(container_root):
-                    checks["flags"]["is_container_change"] = True
-                    checks["dirs"]["containers_to_check"].add(container_root)
-            # Container change
+                    checks_json["flags"]["is_container_change"] = True
+                    checks_json["dirs"]["containers_to_check"].add(container_root)
+            # container change
             if valid_container_dir.match(file["filename"]):
                 container_root = "/".join(file["filename"].split("/")[0:5])
-                checks["flags"]["is_container_change"] = True
-                checks["dirs"]["containers_to_check"].add(container_root)
+                checks_json["flags"]["is_container_change"] = True
+                checks_json["dirs"]["containers_to_check"].add(container_root)
 
-    checks["dirs"]["workloads_to_run"] = list(checks["dirs"]["workloads_to_run"])
-    checks["dirs"]["containers_to_check"] = list(checks["dirs"]["containers_to_check"])
+    checks_json["dirs"]["workloads_to_run"] = list(checks_json["dirs"]["workloads_to_run"])
+    checks_json["dirs"]["containers_to_check"] = list(checks_json["dirs"]["containers_to_check"])
 
-    print(json.dumps(checks))
+    return checks_json
+
+
+def build_filename_diff_url(filename, url): 
+    file_info = {
+        'filename': f'{filename}',
+        'diff_url': ''
+    }
+    
+    filename_hash = hashlib.sha256(filename.encode()).hexdigest()
+    file_info['diff_url'] =  url + '#diff-' + filename_hash
+    
+    return file_info
 
 
 if __name__ == "__main__":
