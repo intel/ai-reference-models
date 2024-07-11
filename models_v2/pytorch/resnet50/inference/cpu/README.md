d36f85b8eefd1fdce6262e6f5f62d28660be5d6f
Minh Le <86155478+cholocate@users.noreply.github.com>
2024-07-10 17:12:33 -0700

    Leminh/resnet50 models v2 (#2278)
    Signed-off-by: Minh1 Le<minh1.le@intel.com>
    Signed-off-by: Mahathi Vatsal <mahathi.vatsal.salopanthula@intel.com>

---
+++ models_v2/pytorch/resnet50/inference/cpu/README.md
@@ -0,0 +1,119 @@
+# Resnet50 Inference
+
+This document has instructions for running ResNet50 inference using
+Intel-optimized PyTorch.
+
+## Model Information
+
+| **Use Case** | **Framework** | **Model Repo** | **Branch/Commit/Tag** | **Optional Patch** |
+|:---:| :---: |:--------------:|:---------------------:|:------------------:|
+|  Inference   |    PyTorch    |       https://github.com/KaimingHe/deep-residual-networks      |           -           |         -          |
+
+## Pre-Requisite
+* Installation of PyTorch and [Intel Extension for PyTorch](https://intel.github.io/intel-extension-for-pytorch/#introduction)
+
+## Bare Metal
+### General setup
+
+Follow [link](https://github.com/IntelAI/models/blob/master/docs/general/pytorch/BareMetalSetup.md) to install Miniconda and build Pytorch, IPEX, TorchVison Jemalloc and TCMalloc.
+
+### Model Specific Setup
+
+* Set Jemalloc and tcmalloc Preload for better performance
+
+  The jemalloc should be built from the [General setup](#general-setup) section.
+  ```
+  export LD_PRELOAD="<path to the jemalloc directory>/lib/libjemalloc.so":"path_to/tcmalloc/lib/libtcmalloc.so":$LD_PRELOAD
+  export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+  ```
+* Set IOMP preload for better performance
+```
+  pip install packaging intel-openmp
+  export LD_PRELOAD=path/lib/libiomp5.so:$LD_PRELOAD
+```
+
+* Set ENV to use fp16 AMX if you are using a supported platform
+```
+  export DNNL_MAX_CPU_ISA=AVX512_CORE_AMX_FP16
+```
+
+* Set ENV for model and dataset path, and optionally run with no network support
+
+## Prepare Dataset
+
+The [ImageNet](http://www.image-net.org/) validation dataset is used when
+testing accuracy. The inference scripts use synthetic data, so no dataset
+is needed.
+
+Download and extract the ImageNet2012 dataset from http://www.image-net.org/,
+then move validation images to labeled subfolders, using
+[the valprep.sh shell script](https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh)
+
+The accuracy script looks for a folder named `val`, so after running the
+data prep script, your folder structure should look something like this:
+
+```
+imagenet
+└── val
+    ├── ILSVRC2012_img_val.tar
+    ├── n01440764
+    │   ├── ILSVRC2012_val_00000293.JPEG
+    │   ├── ILSVRC2012_val_00002138.JPEG
+    │   ├── ILSVRC2012_val_00003014.JPEG
+    │   ├── ILSVRC2012_val_00006697.JPEG
+    │   └── ...
+    └── ...
+```
+
+# Inference
+1. `git clone https://github.com/IntelAI/models.git`
+2. `cd models/models_v2/pytorch/resnet50/inference/cpu`
+3. Create virtual environment `venv` and activate it:
+    ```
+    python3 -m venv venv
+    . ./venv/bin/activate
+    ```
+4. Install the latest CPU versions of [torch, torchvision and intel_extension_for_pytorch](https://intel.github.io/intel-extension-for-pytorch/index.html#installation)
+5. Setup required environment paramaters
+
+| **Parameter**                |                                  **export command**                                  |
+|:---------------------------:|:------------------------------------------------------------------------------------:|
+| **TEST_MODE** (THROUGHPUT, ACCURACY, REALTIME)              | `export TEST_MODE=THROUGHPUT`                  |
+| **DATASET_DIR** (required for ACCURACY)              | `export DATASET_DIR=<path to ImageNet>`                  |
+| **OUTPUT_DIR**               |                               `export OUTPUT_DIR=$PWD`                               |
+| **MODEL_DIR**               |                               `export MODEL_DIR=$(pwd)`                               |
+| **PRECISION**     |                  `export PRECISION=bf16` (fp32, avx-fp32, int8, avx-int8, bf16, fp16, or bf32 for throughput, accuracy, and realtime) |                              |
+| **BATCH_SIZE** (optional)    |                               `export BATCH_SIZE=256`                                |
+| **WEIGHT_SHARING** (optional)              | `export WEIGHT_SHARING=TRUE`                  |
+| **TORCH_INDUCTOR** (optional)              | `export TORCH_INDUCTOR=1`                  |
+6. Run `run_model.sh`
+
+## Output
+
+Inference Throughput output will typically looks like (note that accuracy is measured in throughput and realtime):
+
+```
+inference latency 3.168 ms
+Throughput: 315.657 fps
+Accuracy: 0.000   
+ * Acc@1 0.000 Acc@5 0.000
+```
+Final results of the inference run can be found in `results.yaml` file.
+```
+results:
+ - key: throughput
+   value: 315.657
+   unit: fps
+ - key: latency
+   value: 3.168
+   unit: ms
+ - key: accuracy
+   value: 0.0
+   unit: f1
+```
+
+
+
+
+
+
---
+++ models_v2/pytorch/resnet50/training/cpu/run_model.sh
@@ -0,0 +1,221 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+MODEL_DIR=${MODEL_DIR-$PWD}
+
+if [ ! -e "${MODEL_DIR}/../../common/main.py"  ]; then
+    echo "Could not find the script of main.py. Please set environment variable '\${MODEL_DIR}'."
+    exit 1
+fi
+
+if [ -z "${OUTPUT_DIR}" ]; then
+  echo "The required environment variable OUTPUT_DIR has not been set"
+  exit 1
+fi
+
+if [ -z "${TRAINING_EPOCHS}" ]; then
+  echo "The required environment variable TRAINING_EPOCHS has not been set"
+  exit 1
+fi
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+if [ -z "${DATASET_DIR}" ]; then
+  echo "The required environment variable DATASET_DIR has not been set"
+  exit 1
+fi
+
+if [ ! -d "${DATASET_DIR}" ]; then
+  echo "The DATASET_DIR '${DATASET_DIR}' does not exist"
+  exit 1
+fi
+
+if [ -z "${PRECISION}" ]; then
+  echo "The required environment variable PRECISION has not been set"
+  echo "Please set PRECISION to fp32, avx-fp32, bf16, bf32, or fp16."
+  exit 1
+fi
+
+ARGS=""
+ARGS="$ARGS -a resnet50 ${DATASET_DIR}"
+ARGS_IPEX=""
+
+CORES=`lscpu | grep Core | awk '{print $4}'`
+SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
+TOTAL_CORES=`expr $CORES \* $SOCKETS`
+CORES_PER_INSTANCE=$CORES
+
+if [[ "$DISTRIBUTED" == "true" || "$DISTRIBUTED" == "True" || "$DISTRIBUTED" == "TRUE" ]]; then
+
+    if [ -z "${LOCAL_BATCH_SIZE}" ]; then
+        echo "The required environment variable LOCAL_BATCH_SIZE has not been set"
+        exit 1
+    fi
+
+    if [ -z "${MASTER_ADDR}" ]; then
+        echo "The required environment variable MASTER_ADDR has not been set"
+        exit 1
+    fi
+
+    NNODES=${NNODES:-1}
+    HOSTFILE=${HOSTFILE:-./hostfile}
+    NUM_RANKS=$(( NNODES * SOCKETS ))
+    LOG_PREFIX=resnet50_dist_training_log
+
+    #oneCCL settings
+    export CCL_WORKER_COUNT=8
+    export CCL_LOG_LEVEL=info
+    export CCL_BF16=avx512bf
+    export CCL_ATL_TRANSPORT=ofi
+    export CCL_MNIC_COUNT=2
+    export CCL_MNIC=local
+    export CCL_MNIC_NAME=irdma1,irdma5
+    export CCL_ALLREDUCE=ring
+    export CCL_WORKER_COUNT=8
+
+    for (( i = $SOCKETS; i < 2*$SOCKETS; i++ )); do  # pin CCL workers to HT
+        START_CORE=$(( i * CORES ))
+        for (( j = 0; j < $CCL_WORKER_COUNT; j++)); do
+            CCL_WORKER_AFFINITY="${CCL_WORKER_AFFINITY} $((START_CORE + j))"
+        done
+    done
+
+    export CCL_WORKER_AFFINITY=`echo ${CCL_WORKER_AFFINITY} | tr " " ","`
+
+    #DDP settings
+    export TORCH_CPP_LOG_LEVEL=INFO
+    export TORCH_DISTRIBUTED_DEBUG=INFO
+    export MASTER_ADDR=`head -1 hostfile`
+
+    # Fabric settings
+    export FI_PROVIDER=psm3
+    export PSM3_IDENTIFY=1
+    export PSM3_ALLOW_ROUTERS=1
+    export PSM3_RDMA=1
+    export PSM3_PRINT_STATS=0
+    export PSM3_RV_MR_CACHE_SIZE=8192
+    export PSM3_KASSIST_MODE=none
+    #export PSM3_NIC='irdma*
+    export FI_PSM3_CONN_TIMEOUT=100
+    export PSM3_HAL=sockets
+
+    oneccl_bindings_for_pytorch_path=$(python -c "import torch; import oneccl_bindings_for_pytorch; import os;  print(os.path.abspath(os.path.dirname(oneccl_bindings_for_pytorch.__file__)))")
+    source $oneccl_bindings_for_pytorch_path/env/setvars.sh
+
+    ARGS="$ARGS --warmup-epochs 2 -b $LOCAL_BATCH_SIZE --dist-backend ccl --base-op=LARS --base-lr 10.5 --weight-decay 0.00005"
+    ARGS_IPEX="$ARGS_IPEX --memory-allocator tcmalloc --distributed --nnodes ${NNODES} --hostfile ${HOSTFILE} -logical_cores_for_ccl --ccl_worker_count 8"
+else
+    BATCH_SIZE=${BATCH_SIZE:-128}
+    ARGS_IPEX="$ARGS_IPEX --ninstances 1  --ncore_per_instance ${CORES_PER_INSTANCE} --log_path=${OUTPUT_DIR} --log_file_prefix="./resnet50_training_log_${PRECISION}""
+    ARGS="$ARGS --train-no-eval -w 50 -b $BATCH_SIZE"
+    LOG_PREFIX=resnet50_training_log
+fi
+
+
+if [[ "$PRECISION" == *"avx"* ]]; then
+    unset DNNL_MAX_CPU_ISA
+fi
+
+if [[ $PRECISION == "bf16" ]]; then
+    ARGS="$ARGS --bf16"
+    echo "running bf16 path"
+elif [[ $PRECISION == "bf32" ]]; then
+    ARGS="$ARGS --bf32"
+    echo "running bf32 path"
+elif [[ $PRECISION == "fp16" ]]; then
+    ARGS="$ARGS --fp16"
+    echo "running fp16 path"
+elif [[ $PRECISION == "fp32" || $PRECISION == "avx-fp32" ]]; then
+    echo "running fp32 path"
+else
+    echo "The specified precision '${PRECISION}' is unsupported."
+    echo "Supported precisions are: fp32, avx-fp32, bf16, bf32"
+    exit 1
+fi
+
+ARGS="$ARGS -j 0 --seed 2020 --epochs $TRAINING_EPOCHS"
+
+TORCH_INDUCTOR=${TORCH_INDUCTOR:-"0"}
+
+if [[ "0" == ${TORCH_INDUCTOR} ]];then
+    if [[ -z "${DISTRIBUTED}" ]]; then
+        ARGS="$ARGS --ipex"
+        python -m intel_extension_for_pytorch.cpu.launch \
+            ${ARGS_IPEX} \
+            ${MODEL_DIR}/../../common/main.py \
+            ${ARGS}
+    else
+        python -m intel_extension_for_pytorch.cpu.launch \
+            ${ARGS_IPEX} \
+            ${MODEL_DIR}../../common/train.py \
+            ${ARGS} 2>&1 | tee ${OUTPUT_DIR}/resnet50_dist_training_log_${PRECISION}.log
+    fi
+else
+    export TORCHINDUCTOR_FREEZING=1
+    ARGS="$ARGS --inductor"
+    if [[ -z "${DISTRIBUTED}" ]]; then
+        python -m intel_extension_for_pytorch.cpu.launch \
+            ${ARGS_IPEX} \
+            ${MODEL_DIR}/../../common/main.py \
+            ${ARGS}
+    else
+        python -m intel_extension_for_pytorch.cpu.launch \
+            ${ARGS_IPEX} \
+            ${MODEL_DIR}../../common/train.py \
+            ${ARGS} 2>&1 | tee ${OUTPUT_DIR}/resnet50_dist_training_log_${PRECISION}.log
+    fi
+fi
+
+wait
+
+throughput=$(grep 'Training throughput:'  ${OUTPUT_DIR}/${LOG_PREFIX}_${PRECISION}_* |sed -e 's/.*throughput//;s/[^0-9.]//g' |awk '
+BEGIN {
+    sum = 0;
+    i = 0;
+    }
+    {
+        sum = sum + $1;
+        i++;
+    }
+END   {
+    sum = sum / i;
+    printf("%.3f", sum);
+}')
+echo "--------------------------------Performance Summary per NUMA Node--------------------------------"
+echo "resnet50;"training throughput";${PRECISION};${BATCH_SIZE};${throughput}" | tee -a ${OUTPUT_DIR}/summary.log
+latency="0.0"
+accuracy="0.0"
+
+echo "resnet50;"training throughput";${PRECISION};${BATCH_SIZE};${throughput}" | tee -a ${OUTPUT_DIR}/summary.log
+
+yaml_content=$(cat << EOF
+results:
+- key : throughput
+  value: $throughput
+  unit: examples per second
+- key: latency
+  value: $latency
+  unit: seconds per example
+- key: accuracy
+  value: $accuracy
+  unit: percentage
+EOF
+)
+
+echo "$yaml_content" >  $OUTPUT_DIR/results.yaml
+echo "YAML file created."
