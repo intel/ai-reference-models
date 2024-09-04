85459514fa68d33bf077aa7973a2d9abfe911e2d
Minh Le <86155478+cholocate@users.noreply.github.com>
2024-09-04 16:19:59 -0700

    Implemented Stable Diffusion Pytorch CPU Automation (#2418)

---
+++ tests/cicd/pytorch/stable_diffusion/training/cpu/test_model.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Setup PyTorch Test Enviroment for Stable Diffusion Training"
+
+PRECISION=$1
+OUTPUT_DIR=${OUTPUT_DIR-"$(pwd)/tests/cicd/pytorch/stable_diffusion/training/cpu/output/${PRECISION}"}
+is_lkg_drop=$2
+DISTRIBUTED=$3
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+if [[ "${is_lkg_drop}" == "true" ]]; then
+  source ${WORKSPACE}/pytorch_setup/bin/activate pytorch
+fi
+
+export LD_PRELOAD="${WORKSPACE}/jemalloc/lib/libjemalloc.so":"${WORKSPACE}/tcmalloc/lib/libtcmalloc.so":"/usr/local/lib/libiomp5.so":$LD_PRELOAD
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+export DNNL_MAX_CPU_ISA=AVX512_CORE_AMX
+
+# Install dependency
+cd models_v2/pytorch/stable_diffusion/training/cpu
+MODEL_DIR=$(pwd)
+./setup.sh
+
+# Run script
+OUTPUT_DIR=${OUTPUT_DIR} PRECISION=${PRECISION} DISTRIBUTED=${DISTRIBUTED} ./run_model.sh
+cd -
