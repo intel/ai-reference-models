c5e54f5ee1f6b423d812a9fe5985b24cdbd9c420
Minh Le <86155478+cholocate@users.noreply.github.com>
2024-09-10 13:50:04 -0700

    Implemented MaskRCNN Pytorch CPU Automation (#2422)
    * Implemented MaskRCNN Pytorch CPU Automation
    
    * Fixed linter issues
    
    Co-authored-by: Mahathi Vatsal <mahathi.vatsal.salopanthula@intel.com>

---
+++ tests/cicd/pytorch/maskrcnn/training/cpu/test_model.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+echo "Setup PyTorch Test Enviroment for MaskRCNN Training"
+
+PRECISION=$1
+OUTPUT_DIR=${OUTPUT_DIR-"$(pwd)/tests/cicd/pytorch/maskrcnn/training/cpu/output/${PRECISION}"}
+is_lkg_drop=$2
+DATASET_DIR=$3
+
+# Create the output directory in case it doesn't already exist
+mkdir -p ${OUTPUT_DIR}
+
+if [[ "${is_lkg_drop}" == "true" ]]; then
+  source ${WORKSPACE}/pytorch_setup/bin/activate pytorch
+fi
+
+export LD_PRELOAD="${WORKSPACE}/jemalloc/lib/libjemalloc.so":"${WORKSPACE}/tcmalloc/lib/libtcmalloc.so":"/usr/local/lib/libiomp5.so":$LD_PRELOAD
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+export DNNL_MAX_CPU_ISA=AVX512_CORE_AMX
+
+# Install dependency
+cd models_v2/pytorch/maskrcnn/training/cpu
+MODEL_DIR=$(pwd)
+./setup.sh
+
+cd maskrcnn-benchmark
+pip install -e .
+pip install -r requirements.txt
+
+cd ${MODEL_DIR}
+
+# Run script
+OUTPUT_DIR=${OUTPUT_DIR} PRECISION=${PRECISION} DATASET_DIR=${DATASET_DIR} TEST_MODE=${TEST_MODE} ./run_model.sh
+cd -
