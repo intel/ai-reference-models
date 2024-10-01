e6aeeea72178d1ad3d1295d90d457a734da7814b
mahathis <36486206+Mahathi-Vatsal@users.noreply.github.com>
2024-10-01 10:56:29 -0700

    Fixed llama training issues (#2473)

--- models_v2/pytorch/llama/training/cpu/setup.sh
+++ models_v2/pytorch/llama/training/cpu/setup.sh
@@ -17,13 +17,12 @@
 #
 
 pip install -r requirements.txt
-pip install protobuf==3.20.3 numpy==1.20
+pip install protobuf numpy
 
 # Clone the Transformers repo in the LLAMA2 inference directory
-cd ${MODEL_DIR}
 git clone https://github.com/huggingface/transformers.git
 cd transformers
 git checkout v4.38.1
-git apply ${MODEL_DIR}/../../../../common/enable_ipex_for_transformers.diff
+git apply ../../../../../common/enable_ipex_for_transformers.diff
 pip install -e ./
 cd ..
