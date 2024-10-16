5517c6b0b4592721c22f3494674d298e249a0b84
DiweiSun <105627594+DiweiSun@users.noreply.github.com>
2024-10-17 02:35:46 +0800

    Molly/bkc fix (#2506)
    * chatglm setup bkc fix
    
    * llama training bkc fix
    
    * refine stable-diffusion bkc
    
    * refine dlrm_v2 bkc
    
    ---------
    
    Co-authored-by: Jitendra Patil <jitendra.patil@intel.com>
    Co-authored-by: Srikanth Ramakrishna <srikanth.ramakrishna@intel.com>

--- models_v2/pytorch/chatglm/inference/cpu/setup.sh
+++ models_v2/pytorch/chatglm/inference/cpu/setup.sh
@@ -23,7 +23,7 @@ pip install cpm-kernels
 git clone https://github.com/huggingface/transformers.git
 cd transformers
 git checkout v4.38.1
-git apply ../../../../enable_ipex_for_transformers.diff
+git apply ${MODEL_DIR}/../../../../common/enable_ipex_for_transformers.diff
 pip install -e ./
 cd ..
 
--- models_v2/pytorch/stable_diffusion/inference/cpu/run_model.sh
+++ models_v2/pytorch/stable_diffusion/inference/cpu/run_model.sh
@@ -102,8 +102,6 @@ fi
 
 # Create the output directory in case it doesn't already exist
 mkdir -p ${OUTPUT_DIR}
-rm -rf ${OUTPUT_DIR}/summary.log
-rm -rf ${OUTPUT_DIR}/results.yaml
 
 if [[ "${PRECISION}" == *"avx"* ]]; then
     unset DNNL_MAX_CPU_ISA
