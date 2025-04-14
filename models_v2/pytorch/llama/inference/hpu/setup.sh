701d0bbc8a382f250e933001d1b6e286f9398e30
Tsai, Louie <louie.tsai@intel.com>
2025-04-14 11:33:43 -0700

    setup.sh

--- models_v2/pytorch/llama/inference/hpu/setup.sh
+++ models_v2/pytorch/llama/inference/hpu/setup.sh
@@ -16,18 +16,7 @@
 # limitations under the License.
 #
 
-pip install datasets sentencepiece psutil
 
-# Clone the Transformers repo in the LLAMA2 inference directory
+# Clone the Gaudi Tutorial repo in the LLAMA inference directory
 cd ${MODEL_DIR}
-git clone https://github.com/huggingface/transformers.git
-cd transformers
-git checkout v4.38.1
-git apply ${MODEL_DIR}/../../../../common/enable_ipex_for_transformers.diff
-pip install -e ./
-cd ..
-pip install --no-deps --pre torchao --index-url https://download.pytorch.org/whl/nightly/cpu
-
-# Get prompt.json for gneration inference
-wget https://intel-extension-for-pytorch.s3.amazonaws.com/miscellaneous/llm/prompt.json
-mv prompt.json ${MODEL_DIR}
+git clone https://github.com/HabanaAI/Gaudi-tutorials.git
